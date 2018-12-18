6c3415c23855ff9e6d84c20be23abf0322d18fc7
josepg-limit <josepg@limit.es>
2018-12-18 09:37:58 +0100

    #60: Configuració de permisos per a usuaris
    Task-Url: https://github.com/GovernIB/notib/issues/60

--- notib-war/src/main/java/es/caib/notib/war/controller/ProcedimentPermisController.java
+++ notib-war/src/main/java/es/caib/notib/war/controller/ProcedimentPermisController.java
@@ -1,36 +1,23 @@
 package es.caib.notib.war.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import es.caib.notib.core.api.dto.EntitatDto;
-import es.caib.notib.core.api.dto.PagadorCieDto;
-import es.caib.notib.core.api.dto.PagadorPostalDto;
-import es.caib.notib.core.api.dto.PaginaDto;
-import es.caib.notib.core.api.dto.ProcedimentDto;
 import es.caib.notib.core.api.service.EntitatService;
 import es.caib.notib.core.api.service.GrupService;
 import es.caib.notib.core.api.service.PagadorCieService;
 import es.caib.notib.core.api.service.PagadorPostalService;
 import es.caib.notib.core.api.service.ProcedimentService;
-import es.caib.notib.war.command.PagadorPostalCommand;
-import es.caib.notib.war.command.PagadorPostalFiltreCommand;
-import es.caib.notib.war.command.ProcedimentCommand;
-import es.caib.notib.war.command.ProcedimentFiltreCommand;
-import es.caib.notib.war.helper.DatatablesHelper;
-import es.caib.notib.war.helper.RequestSessionHelper;
-import es.caib.notib.war.helper.RolHelper;
-import es.caib.notib.war.helper.DatatablesHelper.DatatablesResponse;
 
+/**
+ * Controlador per el mantinemnt de permisos de procediments
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ *
+ */
 @Controller
 @RequestMapping("/procediment")
 public class ProcedimentPermisController extends BaseUserController{
