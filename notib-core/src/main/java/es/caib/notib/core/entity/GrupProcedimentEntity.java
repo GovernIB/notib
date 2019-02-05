623dc4b26ec7fea011d3db2f4b8e532166ba72d4
josepg-limit <josepg@limit.es>
2019-02-05 10:42:20 +0100

    #60: Configuració de permisos per a usuaris
    Task-Url: https://github.com/GovernIB/notib/issues/60

---
+++ notib-core/src/main/java/es/caib/notib/core/entity/GrupProcedimentEntity.java
@@ -0,0 +1,77 @@
+package es.caib.notib.core.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.ForeignKey;
+
+import es.caib.notib.core.audit.NotibAuditable;
+
+/**
+ * Classe de model de dades que conté la informació dels grups admesos.
+ * 
+ * @author Limit Tecnologies <limit@limit.es>
+ */
+@Entity
+@Table(name = "not_pro_grup")
+public class GrupProcedimentEntity extends NotibAuditable<Long> {
+
+	@ManyToOne(optional = true, fetch = FetchType.EAGER)
+	@JoinColumn(name = "procediment")
+	@ForeignKey(name = "not_pro_grup_fk")
+	protected ProcedimentEntity procediment;
+	
+	@ManyToOne(optional = true, fetch = FetchType.EAGER)
+	@JoinColumn(name = "grup")
+	@ForeignKey(name = "not_grup_pro_fk")
+	protected GrupEntity grup;
+	
+	
+	public ProcedimentEntity getProcediment() {
+		return procediment;
+	}
+	public GrupEntity getGrup() {
+		return grup;
+	}
+	public void setProcediment(ProcedimentEntity procediment) {
+		this.procediment = procediment;
+	}
+	public void setGrup(GrupEntity grup) {
+		this.grup = grup;
+	}
+	
+	public void update(
+			ProcedimentEntity procediment,
+			GrupEntity grup) {
+		this.procediment = procediment;
+		this.grup = grup;
+	}
+	
+	public static Builder getBuilder(
+			ProcedimentEntity procediment,
+			GrupEntity grup) {
+		return new Builder(
+				procediment,
+				grup);
+	}
+	
+	public static class Builder {
+		GrupProcedimentEntity built;
+		Builder(
+				ProcedimentEntity procediment,
+				GrupEntity grup) {
+			built = new GrupProcedimentEntity();
+			built.procediment = procediment;
+			built.grup = grup;
+		}
+		public GrupProcedimentEntity build() {
+			return built;
+		}
+	}
+	
+	private static final long serialVersionUID = -4924926921877674490L;
+
+}
