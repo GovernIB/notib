09ba9a924a1351ce164a8799f4c0c1ad9d21f37b
Josep Gaya <josepg@lmit.es>
2019-02-20 17:03:53 +0100

    #59: Pantalla de consulta de enviaments
    https://github.com/GovernIB/notib/issues/59

--- notib-client/src/test/java/es/caib/notib/client/ClientRestTest.java
+++ notib-client/src/test/java/es/caib/notib/client/ClientRestTest.java
@@ -28,21 +28,30 @@ import es.caib.notib.ws.notificacio.RespostaAlta;
  */
 public class ClientRestTest extends ClientBaseTest {
 
-	/*private static final String URL = "http://localhost:8180/notib";
+	/*
+	private static final String URL = "http://localhost:8180/notib";
 	private static final String USERNAME = "notapp";
-	private static final String PASSWORD = "notapp";*/
+	private static final String PASSWORD = "notapp";
+	*/
 	private static final String URL = "http://dev.caib.es/notib";
 	private static final String USERNAME = "$ripea_notib";
 	private static final String PASSWORD = "ripea_notib";
 
-	/*@Rule
-	public ExpectedException expectedException = ExpectedException.none();*/
+	/*
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	*/
 
 	private NotificacioRestClient client;
+	private NotificacioRestClient clientV2;
 
 	@Before
 	public void setUp() throws IOException, DecoderException {
 		client = NotificacioRestClientFactory.getRestClient(
+				URL,
+				USERNAME,
+				PASSWORD);
+		clientV2 = NotificacioRestClientFactory.getRestClient(
 				URL,
 				USERNAME,
 				PASSWORD);
@@ -54,6 +63,39 @@ public class ClientRestTest extends ClientBaseTest {
 		String notificacioId = new Long(System.currentTimeMillis()).toString();
 		RespostaAlta respostaAlta = client.alta(
 				generarNotificacio(
+						notificacioId,
+						1,
+						false));
+		if (respostaAlta.isError()) {
+			System.out.println(">>> Reposta amb error: " + respostaAlta.getErrorDescripcio());
+			
+		} else {
+			System.out.println(">>> Reposta Ok");
+		}
+		assertNotNull(respostaAlta);
+		assertFalse(respostaAlta.isError());
+		assertNull(respostaAlta.getErrorDescripcio());
+		assertNotNull(respostaAlta.getReferencies());
+		List<EnviamentReferencia> referencies = respostaAlta.getReferencies();
+		assertEquals(1, referencies.size());
+		assertNotNull(referencies.get(0).getReferencia());
+		assertEquals(
+				NotificacioEstatEnum.ENVIADA,
+				respostaAlta.getEstat());
+		/*RespostaConsultaEstatEnviament respostaConsultaEstatEnviament = client.consultaEstatEnviament(referencies.get(0).getReferencia());
+		assertNotNull(respostaConsultaEstatEnviament);
+		assertFalse(respostaConsultaEstatEnviament.isError());
+		assertNull(respostaConsultaEstatEnviament.getErrorDescripcio());
+		assertEquals(
+				EnviamentEstatEnum.PENDENT_SEU,
+				respostaConsultaEstatEnviament.getEstat());*/
+	}
+	
+	@Test
+	public void testV2() throws DatatypeConfigurationException, IOException, DecoderException {
+		String notificacioId = new Long(System.currentTimeMillis()).toString();
+		RespostaAlta respostaAlta = clientV2.alta(
+				generarNotificacioV2(
 						notificacioId,
 						1,
 						false));
