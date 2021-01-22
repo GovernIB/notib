529a05af7f4bb54715ac1f36e5b01e07a46afccf
josepg-limit <josepg@limit.es>
2021-01-22 23:17:22 +0100

    #432: [4522] Campo descripción tiene caracteres no permitidos: ,

--- notib-core/src/main/java/es/caib/notib/core/helper/NotificaV2Helper.java
+++ notib-core/src/main/java/es/caib/notib/core/helper/NotificaV2Helper.java
@@ -514,7 +514,7 @@ public class NotificaV2Helper extends AbstractNotificaHelper {
 			}
 			envios.setConcepto(notificacio.getConcepte().replace('·', '.'));
 			if (notificacio.getDescripcio() != null)
-				envios.setDescripcion(notificacio.getDescripcio().replace('·', '.'));
+				envios.setDescripcion(notificacio.getDescripcio().replace('·', '.').replace("\r", ""));
 			
 			envios.setProcedimiento(notificacio.getProcedimentCodiNotib());
 			Documento  documento = new Documento();
