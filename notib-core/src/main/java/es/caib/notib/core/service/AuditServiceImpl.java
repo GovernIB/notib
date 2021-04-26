c4ce32d51a03eb5e41681dfcfaefd4c850f51b04
josepg-limit <josepg@limit.es>
2021-04-26 09:48:26 +0200

    Afegir informació estat callback

--- notib-core/src/main/java/es/caib/notib/core/service/AuditServiceImpl.java
+++ notib-core/src/main/java/es/caib/notib/core/service/AuditServiceImpl.java
@@ -1,32 +1,17 @@
 package es.caib.notib.core.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import es.caib.notib.core.api.dto.AplicacioDto;
-import es.caib.notib.core.api.dto.EntitatDto;
-import es.caib.notib.core.api.dto.GrupDto;
-import es.caib.notib.core.api.dto.ProcedimentDto;
-import es.caib.notib.core.api.dto.ProcedimentGrupDto;
+import es.caib.notib.core.api.dto.*;
 import es.caib.notib.core.api.service.AuditService;
 import es.caib.notib.core.entity.NotificacioEntity;
 import es.caib.notib.core.entity.NotificacioEnviamentEntity;
 import es.caib.notib.core.entity.ProcedimentEntity;
-import es.caib.notib.core.entity.auditoria.AplicacioAudit;
-import es.caib.notib.core.entity.auditoria.EntitatAudit;
-import es.caib.notib.core.entity.auditoria.GrupAudit;
-import es.caib.notib.core.entity.auditoria.GrupProcedimentAudit;
-import es.caib.notib.core.entity.auditoria.NotificacioAudit;
-import es.caib.notib.core.entity.auditoria.NotificacioEnviamentAudit;
-import es.caib.notib.core.entity.auditoria.ProcedimentAudit;
-import es.caib.notib.core.repository.auditoria.AplicacioAuditRepository;
-import es.caib.notib.core.repository.auditoria.EntitatAuditRepository;
-import es.caib.notib.core.repository.auditoria.GrupAuditRepository;
-import es.caib.notib.core.repository.auditoria.GrupProcedimentAuditRepository;
-import es.caib.notib.core.repository.auditoria.NotificacioAuditRepository;
-import es.caib.notib.core.repository.auditoria.NotificacioEnviamentAuditRepository;
-import es.caib.notib.core.repository.auditoria.ProcedimentAuditRepository;
+import es.caib.notib.core.entity.auditoria.*;
+import es.caib.notib.core.repository.auditoria.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -48,6 +33,7 @@ public class AuditServiceImpl implements AuditService {
 	private ProcedimentAuditRepository procedimentAuditRepository;
 	
 	@Override
+	@Transactional(propagation = Propagation.MANDATORY)
 	public void audita(
 			Object objecteAuditar,
 			TipusOperacio tipusOperacio,
