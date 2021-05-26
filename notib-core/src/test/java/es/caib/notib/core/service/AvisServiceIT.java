8d7da37794dec29da597ee7f3798cb53ae3d6aa5
josepg <josepg@limit.es>
2021-05-26 15:59:22 +0200

    #286 Afegir un sistema d'avisos generals (más tests integración)

--- notib-core/src/test/java/es/caib/notib/core/service/AvisServiceIT.java
+++ notib-core/src/test/java/es/caib/notib/core/service/AvisServiceIT.java
@@ -3,16 +3,19 @@ package es.caib.notib.core.service;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
-import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.test.context.ContextConfiguration;
@@ -23,10 +26,12 @@ import es.caib.notib.core.api.dto.AvisDto;
 import es.caib.notib.core.api.dto.AvisNivellEnumDto;
 import es.caib.notib.core.api.dto.PaginaDto;
 import es.caib.notib.core.api.dto.PaginacioParamsDto;
-import es.caib.notib.core.api.exception.NotFoundException;
+import es.caib.notib.core.api.dto.PaginacioParamsDto.OrdreDireccioDto;
 import es.caib.notib.core.api.service.AvisService;
 import es.caib.notib.core.repository.AvisRepository;
 import es.caib.notib.core.test.AuthenticationTest;
+import lombok.Getter;
+import lombok.Setter;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/es/caib/notib/core/application-context-test.xml"})
@@ -59,7 +64,7 @@ public class AvisServiceIT {
 		
 	}
 
-//	@Test
+	@Test
 	public void whenCreateAvis_thenCreateTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -80,7 +85,7 @@ public class AvisServiceIT {
 			
 	}
 	
-//	@Test
+	@Test
 	public void whenUpdateAvis_thenUpdateTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -104,7 +109,7 @@ public class AvisServiceIT {
 			
 	}
 	
-//	@Test
+	@Test
 	public void whenUpdateActivaAvisDesactivar_thenUpdateActivaFalseTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -128,7 +133,7 @@ public class AvisServiceIT {
 	
 	}
 	
-//	@Test
+	@Test
 	public void whenDeleteAvis_thenDeleteTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -147,7 +152,7 @@ public class AvisServiceIT {
 	
 	}
 	
-//	@Test
+	@Test
 	public void whenFindByIdAvis_thenReturnTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -170,43 +175,43 @@ public class AvisServiceIT {
 			
 	}
 	
-//	@Test
-//	public void whenFindPaginat_thenReturnPaginaDtoWithTableItems() {
-//		
-//		authenticationTest.autenticarUsuari("super");
-//		// Given:
-//		AvisDto avisCreated1 = avisService.create(avis);
-//		
-//		AvisDto avis2 = new AvisDto();
-//		avis2.setAssumpte("Aviso nivel Warning");
-//		avis2.setMissatge("Se ha desplegado una nueva versión de NOTIB");
-//		avis2.setDataInici(new Date()); // fecha actual
-//		Calendar c = Calendar.getInstance();
-//		c.add(Calendar.DATE, 5);  // se añaden 5 días a la fecha actual
-//		avis2.setDataFinal(c.getTime());
-//		avis2.setAvisNivell(AvisNivellEnumDto.WARNING);
-//		AvisDto avisCreated2 = avisService.create(avis2);
-//		
-//		PaginacioParamsDto paginacioParamsDto = getPaginacioDtoFromRequest(null, null);
-//		
-//		authenticationTest.autenticarUsuari("user");
-//		// When
+	@Test
+	public void whenFindPaginat_thenReturnPaginaDtoWithTableItems() {
+		
+		authenticationTest.autenticarUsuari("super");
+		// Given:
+		AvisDto avisCreated1 = avisService.create(avis);
+		
+		AvisDto avis2 = new AvisDto();
+		avis2.setAssumpte("Aviso nivel Warning");
+		avis2.setMissatge("Se ha desplegado una nueva versión de NOTIB");
+		avis2.setDataInici(new Date()); // fecha actual
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 5);  // se añaden 5 días a la fecha actual
+		avis2.setDataFinal(c.getTime());
+		avis2.setAvisNivell(AvisNivellEnumDto.WARNING);
+		AvisDto avisCreated2 = avisService.create(avis2);
+		
+		PaginacioParamsDto paginacioParamsDto = getPaginacioDtoFromRequest(null, null);
+		
+		authenticationTest.autenticarUsuari("user");
+		// When
 //		PaginaDto<AvisDto> avisosFound = avisService.findPaginat(paginacioParams);
-//		
-//		// Then
-////		assertNotNull(avisosFound);
-////		assertEquals(2, avisosFound.size());
-////		comprobarAvisCoincide(avis, avisosFound.get(0));
-////		comprobarAvisCoincide(avisActive, avisosFound.get(1));
-//		
-//		// Borrado de los elementos creados
-//		authenticationTest.autenticarUsuari("super");
-//		avisService.delete(avisCreated1.getId());
-//		avisService.delete(avisCreated2.getId());
-//		
-//	}
+		
+		// Then
+//		assertNotNull(avisosFound);
+//		assertEquals(2, avisosFound.size());
+//		comprobarAvisCoincide(avis, avisosFound.get(0));
+//		comprobarAvisCoincide(avisActive, avisosFound.get(1));
+		
+		// Borrado de los elementos creados
+		authenticationTest.autenticarUsuari("super");
+		avisService.delete(avisCreated1.getId());
+		avisService.delete(avisCreated2.getId());
+		
+	}
 	
-//	@Test
+	@Test
 	public void whenFindActiveAvis_thenReturnActiveAndUnexpiredTableItems() {
 		
 		authenticationTest.autenticarUsuari("super");
@@ -398,4 +403,94 @@ public class AvisServiceIT {
 		avisService.findActive();
 	}
 	
+	private static PaginacioParamsDto getPaginacioDtoFromRequest(
+			Map<String, String[]> mapeigFiltres,
+			Map<String, String[]> mapeigOrdenacions) {
+		DatatablesParams params = new DatatablesParams();
+		logger.debug("Informació de la pàgina obtingudes de datatables (" +
+				"draw=" + params.getDraw() + ", " +
+				"start=" + params.getStart() + ", " +
+				"length=" + params.getLength() + ")");
+		PaginacioParamsDto paginacio = new PaginacioParamsDto();
+		int paginaNum = params.getStart() / params.getLength();
+		paginacio.setPaginaNum(paginaNum);
+		if (params.getLength() != null && params.getLength().intValue() == -1) {
+			paginacio.setPaginaTamany(Integer.MAX_VALUE);
+		} else {
+			paginacio.setPaginaTamany(params.getLength());
+		}
+		paginacio.setFiltre(params.getSearchValue());
+		for (int i = 0; i < params.getColumnsSearchValue().size(); i++) {
+			String columna = params.getColumnsData().get(i);
+			String[] columnes = new String[] {columna};
+			if (mapeigFiltres != null && mapeigFiltres.get(columna) != null) {
+				columnes = mapeigFiltres.get(columna);
+			}
+			for (String col: columnes) {
+				if (!"<null>".equals(col)) {
+					paginacio.afegirFiltre(
+							col,
+							params.getColumnsSearchValue().get(i));
+					logger.debug("Afegit filtre a la paginació (" +
+							"columna=" + col + ", " +
+							"valor=" + params.getColumnsSearchValue().get(i) + ")");
+				}
+			}
+		}
+		for (int i = 0; i < params.getOrderColumn().size(); i++) {
+			int columnIndex = params.getOrderColumn().get(i);
+			String columna = params.getColumnsData().get(columnIndex);
+			OrdreDireccioDto direccio;
+			if ("asc".equals(params.getOrderDir().get(i)))
+				direccio = OrdreDireccioDto.ASCENDENT;
+			else
+				direccio = OrdreDireccioDto.DESCENDENT;
+			String[] columnes = new String[] {columna};
+			if (mapeigOrdenacions != null && mapeigOrdenacions.get(columna) != null) {
+				columnes = mapeigOrdenacions.get(columna);
+			}
+			for (String col: columnes) {
+				paginacio.afegirOrdre(col, direccio);
+				logger.debug("Afegida ordenació a la paginació (columna=" + columna + ", direccio=" + direccio + ")");
+			}
+		}
+		logger.debug("Informació de la pàgina sol·licitada (paginaNum=" + paginacio.getPaginaNum() + ", paginaTamany=" + paginacio.getPaginaTamany() + ")");
+		return paginacio;
+	}
+	
+	
+	//arreglarlo para avisos, estaba para listado de procediments
+	@Getter @Setter
+	protected static class DatatablesParams {
+		private Integer draw;
+		private Integer start;
+		private Integer length;
+		private String searchValue;
+		private Boolean searchRegex;
+		private List<Integer> orderColumn = new ArrayList<Integer>();
+		private List<String> orderDir = new ArrayList<String>();
+		private List<String> columnsData = new ArrayList<String>();
+		private List<String> columnsName = new ArrayList<String>();
+		private List<Boolean> columnsSearchable = new ArrayList<Boolean>();
+		private List<Boolean> columnsOrderable = new ArrayList<Boolean>();
+		private List<String> columnsSearchValue = new ArrayList<String>();
+		private List<Boolean> columnsSearchRegex = new ArrayList<Boolean>();
+		protected DatatablesParams() {
+			draw = 1;
+			start = 0;
+			length = 10;
+			searchValue = "";
+			searchRegex = null;
+			orderColumn.add(3);
+			orderDir.add("desc");
+			columnsData = Arrays.asList("id", "codi", "nom", "organGestorDesc", "pagadorpostal", "pagadorcie", "comu", "agrupar", "grupsCount", "permisosCount", "id");
+			columnsName = Arrays.asList(null, null, null, null, null, null, null, null, null, null, null);
+			columnsSearchable = Arrays.asList(false, false, false, false, false, false, false, false, false, false, false);
+			columnsOrderable = Arrays.asList(false, false, false, false, false, false, false, false, false, false, false);
+			columnsSearchValue = Arrays.asList(null, null, null, null, null, null, null, null, null, null, null);
+			columnsSearchRegex = Arrays.asList(false, false, false, false, false, false, false, false, false, false, false);
+		}
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(AvisServiceIT.class);
 }
