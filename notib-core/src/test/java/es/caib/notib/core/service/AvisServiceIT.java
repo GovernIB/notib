8ae1d6fdb83c649b7429e35f0d85660f04858240
josepg <josepg@limit.es>
2021-05-26 13:52:12 +0200

    #286 Afegir un sistema d'avisos generals (añadir test integración)

--- notib-core/src/test/java/es/caib/notib/core/service/AvisServiceIT.java
+++ notib-core/src/test/java/es/caib/notib/core/service/AvisServiceIT.java
@@ -2,11 +2,11 @@ package es.caib.notib.core.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -43,45 +43,225 @@ public class AvisServiceIT {
 	AvisRepository avisRepository;
 	
 	
-//	private AvisDto avisDto;
+	private AvisDto avis;
 	
 	@Before
 	public void setUp() {
+		
+		avis = new AvisDto();
+		avis.setAssumpte("Aviso nivel Información");
+		avis.setMissatge("Se ha desplegado una nueva versión de NOTIB");
+		avis.setDataInici(new Date()); // fecha actual
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 5);  // se añaden 5 días a la fecha actual
+		avis.setDataFinal(c.getTime());
+		avis.setAvisNivell(AvisNivellEnumDto.INFO);
+		
 	}
 
-	@Test
+//	@Test
 	public void whenCreateAvis_thenCreateTableItem() {
 		
 		authenticationTest.autenticarUsuari("super");
 		
-		// Given
-		AvisDto avisDto = new AvisDto();
-		avisDto.setAssumpte("Aviso nivel Información");
-		avisDto.setMissatge("Se ha desplegado una nueva versión de NOTIB");
-		avisDto.setDataInici(new Date());
-		SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		try {
-			avisDto.setDataFinal(formatoFecha.parse("2021-06-25 00:00:00"));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-//		avisDto.setActiu(null);
-		avisDto.setAvisNivell(AvisNivellEnumDto.INFO);
+		// Given: AvisDto ya creado en setUp()
 		
 		// When
-		AvisDto avisCreated = avisService.create(avisDto);
+		AvisDto avisCreated = avisService.create(avis);
 		
 		// Then
-		try {
-			assertNotNull(avisCreated);
-			assertNotNull(avisCreated.getId());
-			comprobarAvisCoincide(avisDto, avisCreated);
-			assertEquals(avisCreated.getActiu(), true);
-		} finally {
-			// Borrado de los elementos creados
-			avisService.delete(avisCreated.getId());
-		}
+		assertNotNull(avisCreated);
+		assertNotNull(avisCreated.getId());
+		comprobarAvisCoincide(avis, avisCreated);
+		assertEquals(true, avisCreated.getActiu());
+		
+		// Borrado de los elementos creados
+		avisService.delete(avisCreated.getId());
+			
+	}
 	
+//	@Test
+	public void whenUpdateAvis_thenUpdateTableItem() {
+		
+		authenticationTest.autenticarUsuari("super");
+		
+		// Given: un aviso existente
+		AvisDto avisCreated = avisService.create(avis);
+		avis.setId(avisCreated.getId());
+		
+		// When
+		AvisDto avisUpdated = avisService.update(avis);
+		
+		// Then
+		assertNotNull(avisUpdated);
+		assertNotNull(avisUpdated.getId());
+		assertEquals(avisCreated.getId(), avisUpdated.getId());
+		comprobarAvisCoincide(avis, avisUpdated);
+		assertEquals(true, avisUpdated.getActiu());
+
+		// Borrado de los elementos creados
+		avisService.delete(avisCreated.getId());
+			
+	}
+	
+//	@Test
+	public void whenUpdateActivaAvisDesactivar_thenUpdateActivaFalseTableItem() {
+		
+		authenticationTest.autenticarUsuari("super");
+		
+		// Given: un aviso existente y activo
+		AvisDto avisCreated = avisService.create(avis);
+		avis.setId(avisCreated.getId());
+		
+		// When: lo desactivo
+		AvisDto avisUpdated = avisService.updateActiva(avis.getId(), false);
+		
+		// Then
+		assertNotNull(avisUpdated);
+		assertNotNull(avisUpdated.getId());
+		assertEquals(avisCreated.getId(), avisUpdated.getId());
+		comprobarAvisCoincide(avis, avisUpdated);
+		assertEquals(false, avisUpdated.getActiu());
+		
+		// Borrado de los elementos creados
+		avisService.delete(avisCreated.getId());
+	
+	}
+	
+//	@Test
+	public void whenDeleteAvis_thenDeleteTableItem() {
+		
+		authenticationTest.autenticarUsuari("super");
+		
+		// Given: un aviso existente
+		AvisDto avisCreated = avisService.create(avis);
+		
+		// When
+		AvisDto avisDeleted = avisService.delete(avisCreated.getId());
+		
+		// Then
+		comprobarAvisCoincide(avis, avisDeleted);
+		authenticationTest.autenticarUsuari("user");
+		AvisDto avisFound = avisService.findById(avisCreated.getId());
+		assertNull(avisFound);
+	
+	}
+	
+//	@Test
+	public void whenFindByIdAvis_thenReturnTableItem() {
+		
+		authenticationTest.autenticarUsuari("super");
+		
+		// Given: un aviso existente
+		AvisDto avisCreated = avisService.create(avis);
+		
+		authenticationTest.autenticarUsuari("user");
+		// When
+		AvisDto avisFound = avisService.findById(avisCreated.getId());
+		
+		// Then
+		assertNotNull(avisFound);
+		assertNotNull(avisFound.getId());
+		comprobarAvisCoincide(avis, avisFound);
+		
+		// Borrado de los elementos creados
+		authenticationTest.autenticarUsuari("super");
+		avisService.delete(avisCreated.getId());
+			
+	}
+	
+//	@Test
+//	public void whenFindPaginat_thenReturnPaginaDtoWithTableItems() {
+//		
+//		authenticationTest.autenticarUsuari("super");
+//		// Given:
+//		AvisDto avisCreated1 = avisService.create(avis);
+//		
+//		AvisDto avis2 = new AvisDto();
+//		avis2.setAssumpte("Aviso nivel Warning");
+//		avis2.setMissatge("Se ha desplegado una nueva versión de NOTIB");
+//		avis2.setDataInici(new Date()); // fecha actual
+//		Calendar c = Calendar.getInstance();
+//		c.add(Calendar.DATE, 5);  // se añaden 5 días a la fecha actual
+//		avis2.setDataFinal(c.getTime());
+//		avis2.setAvisNivell(AvisNivellEnumDto.WARNING);
+//		AvisDto avisCreated2 = avisService.create(avis2);
+//		
+//		PaginacioParamsDto paginacioParamsDto = getPaginacioDtoFromRequest(null, null);
+//		
+//		authenticationTest.autenticarUsuari("user");
+//		// When
+//		PaginaDto<AvisDto> avisosFound = avisService.findPaginat(paginacioParams);
+//		
+//		// Then
+////		assertNotNull(avisosFound);
+////		assertEquals(2, avisosFound.size());
+////		comprobarAvisCoincide(avis, avisosFound.get(0));
+////		comprobarAvisCoincide(avisActive, avisosFound.get(1));
+//		
+//		// Borrado de los elementos creados
+//		authenticationTest.autenticarUsuari("super");
+//		avisService.delete(avisCreated1.getId());
+//		avisService.delete(avisCreated2.getId());
+//		
+//	}
+	
+//	@Test
+	public void whenFindActiveAvis_thenReturnActiveAndUnexpiredTableItems() {
+		
+		authenticationTest.autenticarUsuari("super");
+		// Given:
+		// Aviso activo y no expirado
+		AvisDto avisCreated = avisService.create(avis);
+		
+		// Otro Aviso activo y no expirado
+		AvisDto avisActive = new AvisDto(); 
+		avisActive.setAssumpte("Aviso nivel Warning");
+		avisActive.setMissatge("Se ha desplegado una nueva versión de NOTIB");
+		avisActive.setDataInici(new Date());
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 5);
+		avisActive.setDataFinal(c.getTime());
+		avisActive.setAvisNivell(AvisNivellEnumDto.WARNING);
+		AvisDto avisActiveCreated = avisService.create(avisActive);
+		
+		// Aviso no activo y no expirado
+		AvisDto avisNoActive = new AvisDto(); 
+		avisNoActive.setAssumpte("Aviso nivel Error 1");
+		avisNoActive.setMissatge("Se ha desplegado una nueva versión de NOTIB con errores");
+		avisNoActive.setDataInici(new Date());
+		avisNoActive.setDataFinal(c.getTime());
+		avisNoActive.setAvisNivell(AvisNivellEnumDto.ERROR);
+		AvisDto avisNoActiveCreated = avisService.create(avisNoActive); 
+		avisNoActiveCreated = avisService.updateActiva(avisNoActiveCreated.getId(), false);
+		
+		// Aviso expirado
+		AvisDto avisExpired = new AvisDto();
+		avisExpired.setAssumpte("Aviso nivel Error 2");
+		avisExpired.setMissatge("Se ha desplegado una nueva versión de NOTIB con errores");
+		c.add(Calendar.DATE, -10);
+		avisExpired.setDataInici(c.getTime());
+		avisExpired.setDataFinal(c.getTime());
+		avisExpired.setAvisNivell(AvisNivellEnumDto.ERROR);
+		AvisDto avisExpiredCreated = avisService.create(avisExpired);
+		
+		authenticationTest.autenticarUsuari("user");
+		// When
+		List<AvisDto> avisosFound = avisService.findActive();
+		
+		// Then: devuelve los avisos activos que no hayan han expirado
+		assertNotNull(avisosFound);
+		assertEquals(2, avisosFound.size());
+		comprobarAvisCoincide(avis, avisosFound.get(0));
+		comprobarAvisCoincide(avisActive, avisosFound.get(1));
+		
+		// Borrado de los elementos creados
+		authenticationTest.autenticarUsuari("super");
+		avisService.delete(avisCreated.getId());
+		avisService.delete(avisActiveCreated.getId());
+		avisService.delete(avisNoActiveCreated.getId());
+		avisService.delete(avisExpiredCreated.getId());
+			
 	}
 	
 	private void comprobarAvisCoincide(AvisDto esperado, AvisDto actual) {
@@ -90,6 +270,132 @@ public class AvisServiceIT {
 		assertEquals(esperado.getDataInici(), actual.getDataInici());
 		assertEquals(esperado.getDataFinal(), actual.getDataFinal());
 		assertEquals(esperado.getAvisNivell(), actual.getAvisNivell());			
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesUserCreate() {
+		authenticationTest.autenticarUsuari("user");
+		avisService.create(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminCreate() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.create(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplCreate() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.create(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesUserUpdate() {
+		authenticationTest.autenticarUsuari("user");
+		avisService.update(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminUpdate() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.update(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplUpdate() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.update(avis);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesUserUpdateActiva() {
+		authenticationTest.autenticarUsuari("user");
+		avisService.updateActiva(avis.getId(), false);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminUpdateActiva() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.updateActiva(avis.getId(), false);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplUpdateActiva() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.updateActiva(avis.getId(), false);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesUserDelete() {
+		authenticationTest.autenticarUsuari("user");
+		avisService.delete(avis.getId());
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminDelete() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.delete(avis.getId());
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplDelete() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.delete(avis.getId());
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesSuperFindById() {
+		authenticationTest.autenticarUsuari("super");
+		avisService.findById(avis.getId());
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminFindById() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.findById(avis.getId());
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplFindById() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.findById(avis.getId());
+	}
+
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesSuperFindPaginat() {
+		authenticationTest.autenticarUsuari("super");
+		avisService.findPaginat(null);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminFindPaginat() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.findPaginat(null);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplFindPaginat() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.findPaginat(null);
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesSuperFindActive() {
+		authenticationTest.autenticarUsuari("super");
+		avisService.findActive();
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAdminFindActive() {
+		authenticationTest.autenticarUsuari("admin");
+		avisService.findActive();
+	}
+	
+	@Test(expected = AccessDeniedException.class)
+	public void errorSiAccesAplFindActive() {
+		authenticationTest.autenticarUsuari("apl");
+		avisService.findActive();
 	}
 	
 }
