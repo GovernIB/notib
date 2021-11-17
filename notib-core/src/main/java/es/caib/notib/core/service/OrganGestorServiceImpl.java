71b7377b4146c9afa8d9240074a8aaa043abea44
josepg-limit <josepg@limit.es>
2021-11-17 11:18:34 +0100

    findByCodisAndEstat arreclar problema de in amb oracle

--- notib-core/src/main/java/es/caib/notib/core/service/OrganGestorServiceImpl.java
+++ notib-core/src/main/java/es/caib/notib/core/service/OrganGestorServiceImpl.java
@@ -289,11 +289,16 @@ public class OrganGestorServiceImpl implements OrganGestorService{
 	@Override
 	@Transactional(readOnly = true)
 	public List<OrganGestorDto> findByCodisAndEstat(List<String> codisOrgans, OrganGestorEstatEnum estat) {
+
 		Timer.Context timer = metricsHelper.iniciMetrica();
 		try {
-			return conversioTipusHelper.convertirList(
-					organGestorRepository.findByEstatAndCodiIn(codisOrgans, estat),
-					OrganGestorDto.class);
+			List<OrganGestorEntity> organs = new ArrayList<>();
+			int chunkSize = 100;
+			for (int foo = 0; foo < codisOrgans.size(); foo=foo+chunkSize) {
+				List<OrganGestorEntity> organsChunk = organGestorRepository.findByEstatAndCodiIn(codisOrgans.subList(foo, chunkSize), estat);
+				organs.addAll(organsChunk);
+			}
+			return conversioTipusHelper.convertirList(organs, OrganGestorDto.class);
 		} finally {
 			metricsHelper.fiMetrica(timer);
 		}
