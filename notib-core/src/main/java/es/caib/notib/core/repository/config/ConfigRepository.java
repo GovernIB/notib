422097b709705e60adc58214228eb508ec8cfa1a
josepg-limit <josepg@limit.es>
2022-04-28 14:54:14 +0200

    #693 Propietats configurables a nivell d'entitat

--- notib-core/src/main/java/es/caib/notib/core/repository/config/ConfigRepository.java
+++ notib-core/src/main/java/es/caib/notib/core/repository/config/ConfigRepository.java
@@ -24,7 +24,8 @@ public interface ConfigRepository extends JpaRepository<ConfigEntity, String> {
 
     List<ConfigEntity> findByEntitatCodiIsNull();
 
-    List<ConfigEntity> findByKeyContainsKeyAndEntitatCodiIsNotNull(String key);
+    @Query("FROM ConfigEntity c WHERE c.key = :key AND c.entitatCodi IS NOT NULL")
+    List<ConfigEntity> findByKeyContainsKeyAndEntitatCodiIsNotNull(@Param("key") String key);
 
     @Transactional
     @Modifying
