4f46cc5dab92fd205752f84f51ea66c04b9301ef
josepg-limit <josepg@limit.es>
2022-09-01 17:06:09 +0200

    ddl i dml

---
+++ versiones/v1.1.21/oracle/02_update_1.1.21_dml.sql
@@ -0,0 +1,3 @@
+UPDATE not_procediment SET organ_no_sinc = 0;
+
+INSERT INTO not_config (key, value, description, group_code, position, jboss_property, type_code) VALUES ('es.caib.notib.organs.consulta.canvis', NULL, 'Cron per a la consulta de canvis a l''organigrama', 'SCHEDULLED', '2', '0', 'TEXT');
