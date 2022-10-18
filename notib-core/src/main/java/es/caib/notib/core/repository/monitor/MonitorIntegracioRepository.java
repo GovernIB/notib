0aa64170b841bf339f0c38742260ce2155526433
josepg-limit <josepg@limit.es>
2022-10-18 15:20:23 +0200

    #734 Posar estat vigent per defecte en el filtre del llistat d'òrgans gestors

--- notib-core/src/main/java/es/caib/notib/core/repository/monitor/MonitorIntegracioRepository.java
+++ notib-core/src/main/java/es/caib/notib/core/repository/monitor/MonitorIntegracioRepository.java
@@ -3,14 +3,29 @@ package es.caib.notib.core.repository.monitor;
 import es.caib.notib.core.api.dto.IntegracioAccioEstatEnumDto;
 import es.caib.notib.core.entity.monitor.MonitorIntegracioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface MonitorIntegracioRepository extends JpaRepository<MonitorIntegracioEntity, Long> {
 
 
-    List<MonitorIntegracioEntity> findAllByCodi(@Param("codi") String codi);
+    @Query("from MonitorIntegracioEntity n " +
+            "where lower(n.codi) like concat('%', lower(:codi),'%') " +
+            "and (:isCodiEntitatNull = true or lower(n.codiEntitat) like concat('%', lower(:codiEntitat), '%')) " +
+            "and (:isAplicacioNull = true or lower(n.aplicacio) like concat('%', lower(:aplicacio), '%')) " +
+            "order by n.data desc ")
+    List<MonitorIntegracioEntity> getByFiltre(@Param("codi") String codi,
+                                              @Param("isCodiEntitatNull") boolean isCodiEntitatNull,
+                                              @Param("codiEntitat") String codiEntitat,
+                                              @Param("isAplicacioNull") boolean isAplicacioNull,
+                                              @Param("aplicacio") String aplicacio);
+
+    List<MonitorIntegracioEntity> findAllByCodiOrderByDataDesc(@Param("codi") String codi);
 
     int countByCodiAndEstat(@Param("codi") String codi, @Param("estat")IntegracioAccioEstatEnumDto estat);
+
+    void deleteByDataIsBefore(@Param("llindar") Date llindar);
 }
