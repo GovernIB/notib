62029130900fc3e708a129aab6f634137e4e5468
josepg-limit <josepg@limit.es>
2022-11-07 17:40:14 +0100

    #607 Vista prèvia de sincronització del dir3 i còpia de permisos d'òrgan

--- notib-core/src/main/java/es/caib/notib/core/helper/IntegracioHelper.java
+++ notib-core/src/main/java/es/caib/notib/core/helper/IntegracioHelper.java
@@ -132,6 +132,11 @@ public class IntegracioHelper {
 		addAccioError(info, errorDescripcio, null,true);
 	}
 
+	public void addAccioError(IntegracioInfo info, String errorDescripcio, boolean obtenirUsuari) {
+
+		addAccioError(info, errorDescripcio, null,true);
+	}
+
 	public void addAccioError(IntegracioInfo info, String errorDescripcio, Throwable throwable) {
 
 		addAccioError(info, errorDescripcio, throwable,true);
