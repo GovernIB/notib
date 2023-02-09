568f50e9d1791db0cfb37d1b86af6b119a3ec4dd
josepg-limit <josepg@limit.es>
2023-02-09 21:07:24 +0100

    Errors producció: Error obtenir oficines plugin WS

--- notib-plugin-impl/src/main/java/es/caib/notib/plugin/unitat/UnitatsOrganitzativesPluginDir3Ws.java
+++ notib-plugin-impl/src/main/java/es/caib/notib/plugin/unitat/UnitatsOrganitzativesPluginDir3Ws.java
@@ -544,6 +544,7 @@ public class UnitatsOrganitzativesPluginDir3Ws implements UnitatsOrganitzativesP
 				OficinaSir oficinaSIR = new OficinaSir();
 				oficinaSIR.setCodi(oficinaTF.getCodigo());
 				oficinaSIR.setNom(oficinaTF.getDenominacion());
+				oficinaSIR.setOrganCodi(oficinaTF.getCodUoResponsable());
 				oficinaSIR.setSir(oficinaTF.getSirOfi() != null && !oficinaTF.getSirOfi().isEmpty());
 				oficinesSIR.add(oficinaSIR);
 			}
