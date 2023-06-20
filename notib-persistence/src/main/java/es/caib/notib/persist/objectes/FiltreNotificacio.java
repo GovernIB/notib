6cd31a855c62b6cc716b0ae4d4debfb7245b55ef
josepg-limit <josepg@limit.es>
2023-06-20 17:34:02 +0200

    Refactor filtre notificacions

---
+++ notib-persistence/src/main/java/es/caib/notib/persist/objectes/FiltreNotificacio.java
@@ -0,0 +1,12 @@
+package es.caib.notib.persist.objectes;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FiltreNotificacio {
+
+    private boolean isEntitatIdNull;
+    private Long entitatId;
+}
