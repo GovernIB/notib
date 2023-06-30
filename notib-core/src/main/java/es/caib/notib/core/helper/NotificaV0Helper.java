2261fa821500df1a77e1c24985b01ca78bc63156
josepg-limit <josepg@limit.es>
2023-06-30 09:03:59 +0200

    La previsuaització de sincronització d'òrgans no mostra el resultat esperat

--- notib-core/src/main/java/es/caib/notib/core/helper/NotificaV0Helper.java
+++ notib-core/src/main/java/es/caib/notib/core/helper/NotificaV0Helper.java
@@ -97,7 +97,8 @@ public class NotificaV0Helper extends AbstractNotificaHelper {
 				//Crea un nou event
 				for (ResultadoEnvio resultadoEnvio: resultadoAlta.getResultadoEnvios().getItem()) {
 					for (NotificacioEnviamentEntity enviament: notificacio.getEnviamentsPerNotifica()) {
-						if (enviament.getTitular() != null && enviament.getTitular().getNif().equalsIgnoreCase(resultadoEnvio.getNifTitular())) {
+						String nif = enviament.getTitular().isIncapacitat() ? enviament.getDestinataris().get(0).getNif() : enviament.getTitular().getNif();
+						if (enviament.getTitular() != null && nif.equalsIgnoreCase(resultadoEnvio.getNifTitular())) {
 							enviament.updateNotificaEnviada(resultadoEnvio.getIdentificador());
 							enviamentTableHelper.actualitzarRegistre(enviament);
 							enviamentHelper.auditaEnviament(enviament, AuditService.TipusOperacio.UPDATE, "NotificaV0Helper.notificacioEnviar");
@@ -405,7 +406,13 @@ public class NotificaV0Helper extends AbstractNotificaHelper {
 		ResultadoEnvios resultadoEnvios = new ResultadoEnvios();
 		for (NotificacioEnviamentEntity enviament: notificacio.getEnviaments()) {
 			ResultadoEnvio resultatEnviament = new ResultadoEnvio();
-			resultatEnviament.setNifTitular(enviament.getTitular().getNif());
+//			resultatEnviament.setNifTitular(enviament.getTitular().getNif());
+
+			if (enviament.getTitular().isIncapacitat() && enviament.getDestinataris() != null) {
+				resultatEnviament.setNifTitular(enviament.getDestinataris().get(0).getNif());
+			} else {
+				resultatEnviament.setNifTitular(enviament.getTitular().getNif());
+			}
 			resultatEnviament.setIdentificador(getRandomAlphaNumericString(20));
 			resultadoEnvios.getItem().add(resultatEnviament);
 		}
