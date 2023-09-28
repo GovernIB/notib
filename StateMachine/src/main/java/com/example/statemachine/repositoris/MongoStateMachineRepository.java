7f272b467441398f772b4639d292e3aa7127baac
josepg-limit <josepg@limit.es>
2023-09-28 16:19:08 +0200

    Sciprts de versió

--- StateMachine/src/main/java/com/example/statemachine/repositoris/MongoStateMachineRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.statemachine.repositoris;
-
-import org.springframework.statemachine.data.mongodb.MongoDbStateMachineRepository;
-
-public interface MongoStateMachineRepository extends MongoDbStateMachineRepository {
-}
