faf3947be6a526c6efb22e1a076890a302c47920
josepg-limit <josepg@limit.es>
2024-02-23 15:47:16 +0100

    Dates com a Long al swagger

--- notib-api-interna/src/main/java/es/caib/notib/api/interna/openapi/model/notificacio/DatatApi.java
+++ notib-api-interna/src/main/java/es/caib/notib/api/interna/openapi/model/notificacio/DatatApi.java
@@ -16,27 +16,19 @@ import java.util.Date;
 @Schema(name = "Datat")
 public class DatatApi {
 
-    @Schema(name = "estat", implementation = EnviamentEstat.class, example = "NOTIFICADA",
-            description = "Estat de la notificació")
+    @Schema(name = "estat", implementation = EnviamentEstat.class, example = "NOTIFICADA", description = "Estat de la notificació")
     private EnviamentEstat estat;
-    @Schema(name = "data", implementation = Date.class, example = "2023-05-29T07:32:03.526+0000",
-            description = "Data en que s'ha creat el datat")
+    @Schema(name = "data", implementation = Long.class, example = "1706168093962", description = "Data en que s'ha creat el datat")
     private Date data;
-    @Schema(name = "origen", implementation = String.class, example = "carpeta",
-            description = "Origen del datat")
+    @Schema(name = "origen", implementation = String.class, example = "carpeta", description = "Origen del datat")
     private String origen;
-    @Schema(name = "receptorNif", implementation = String.class, example = "00000000T",
-            description = "Nif del receptor de la notificació")
+    @Schema(name = "receptorNif", implementation = String.class, example = "00000000T", description = "Nif del receptor de la notificació")
     private String receptorNif;
-    @Schema(name = "receptorNom", implementation = String.class, example = "Pep Riera",
-            description = "Nom del receptor de la notificació")
+    @Schema(name = "receptorNom", implementation = String.class, example = "Pep Riera", description = "Nom del receptor de la notificació")
     private String receptorNom;
-    @Schema(name = "errorDescripcio", implementation = String.class, example = "java.lang.NullPointerException",
-            description = "Descripció de l'error, en cas que es datat tingui algun error")
+    @Schema(name = "errorDescripcio", implementation = String.class, example = "java.lang.NullPointerException", description = "Descripció de l'error, en cas que es datat tingui algun error")
     private String errorDescripcio;
-    @Schema(name = "numSeguiment", implementation = String.class, example = "123456789",
-            description = "Número de seguiment, en cas d'enviament postal\n" +
-                    " * Camp actualemnt no utilitzat")
+    @Schema(name = "numSeguiment", implementation = String.class, example = "123456789", description = "Número de seguiment, en cas d'enviament postal\n * Camp actualemnt no utilitzat")
     private String numSeguiment;
 
 }
--- notib-api-interna/src/main/java/es/caib/notib/api/interna/openapi/model/notificacio/RespostaBaseApi.java
+++ notib-api-interna/src/main/java/es/caib/notib/api/interna/openapi/model/notificacio/RespostaBaseApi.java
@@ -9,14 +9,11 @@ import java.util.Date;
 @Getter
 public class RespostaBaseApi {
 
-    @Schema(name = "error", implementation = Boolean.class, example = "true",
-            description = "Indica si s'ha produït algún error en l'enviament")
+    @Schema(name = "error", implementation = Boolean.class, example = "true", description = "Indica si s'ha produït algún error en l'enviament")
     private boolean error;
-    @Schema(name = "errorData", implementation = Date.class, example = "2023-05-29T07:32:03.526+0000",
-            description = "Data en que s'ha produït l'error")
+    @Schema(name = "errorData", implementation = Long.class, example = "1706168093962", description = "Data en que s'ha produït l'error")
     private Date errorData;
-    @Schema(name = "errorDescripcio", implementation = String.class, example = "java.lang.NullPointerException",
-            description = "Identificador de la notificació a Notib")
+    @Schema(name = "errorDescripcio", implementation = String.class, example = "java.lang.NullPointerException", description = "Identificador de la notificació a Notib")
     private String errorDescripcio;
 
 }
