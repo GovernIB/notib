65170c3abe0d19199c203e8595658314bce8cc97
josepg-limit <josepg@limit.es>
2024-05-08 09:31:00 +0200

    #860 Revisar les accions massives de la consulta de remeses

---
+++ scripts/bbdd/oracle/2.0.3/updates/01_update_2.0.2_a_2.0.3_ddl.sql
@@ -0,0 +1,2 @@
+alter table not_notificacio_table add DELETED NUMBER(1,0) DEFAULT 0;
+alter table not_notificacio add DELETED NUMBER(1,0) DEFAULT 0;
---
+++ versiones/v2.0.3/postgres/01_update_2.0.3_ddl.sql
@@ -0,0 +1,2 @@
+alter table not_notificacio_table add DELETED BOOLEAN DEFAULT '0';
+alter table not_notificacio add DELETED BOOLEAN DEFAULT '0';
