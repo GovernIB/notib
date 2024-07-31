1a1d3e85d556f9e989affa3dd926d55828b27279
josepg-limit <josepg@limit.es>
2024-07-31 11:02:17 +0200

    Scripts versio

---
+++ scripts/bbdd/oracle/2.0.5/updates/02_update_2.0.4_a_2.0.5_dml.sql
@@ -0,0 +1,1 @@
+INSERT INTO NOT_CONFIG (KEY, VALUE, DESCRIPTION, GROUP_CODE, POSITION, JBOSS_PROPERTY, TYPE_CODE, CONFIGURABLE) VALUES ('es.caib.notib.plugin.gesdoc.document.llindar.dies.comprimir', '10', 'Comprimeix els documents anteriors al valor en dies', 'GES_DOC', 0, 0, 'INT', 0);
