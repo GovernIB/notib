d5b9939b3d8dc782f49a7842f37525cf638926fa
josepg-limit <josepg@limit.es>
2024-10-28 09:37:24 +0100

    #889 No permetre fer enviaments des d'aplicacions inexistents o inactives

--- notib-service/src/main/java/es/caib/notib/logic/helper/IntegracioHelper.java
+++ notib-service/src/main/java/es/caib/notib/logic/helper/IntegracioHelper.java
@@ -178,7 +178,7 @@ public class IntegracioHelper {
 				log.warn("Error IntegracioHelper.getUsuariNomCodi -> Usuari " + auth.getName() + " no trobat a la bbdd");
 				return usuariNomCodi;
 			}
-			if (Strings.isNullOrEmpty(accio.getAplicacio())) {
+			if (!Strings.isNullOrEmpty(accio.getAplicacio())) {
 				return usuari.getNomSencer() + " (" + usuari.getCodi() + ")";
 			}
 			var aplicacio = aplicacioRepository.findByUsuariCodi(usuari.getCodi());
