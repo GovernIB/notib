e7dfa3733eca6a9ad37e9c4551bb591a5abf9762
josepg-limit <josepg@limit.es>
2025-08-25 13:11:51 +0200

    Integracio amb Comanda

--- notib-service-intf/src/main/java/es/caib/notib/logic/intf/dto/permis/PermisProcSer.java
+++ notib-service-intf/src/main/java/es/caib/notib/logic/intf/dto/permis/PermisProcSer.java
@@ -4,12 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Builder
 @Getter
 @Setter
-public class PermisProcSer {
+public class PermisProcSer implements Serializable {
 
     private List<String> usuarisAmbPermis;
     private List<String> rolsAmbPermis;
