8b8ea1a761af651fa03b848d59af4977d6012dcb
josepg-limit <josepg@limit.es>
2025-11-07 09:05:59 +0100

    Enviament de logs a Comanda

--- notib-service-intf/src/main/java/es/caib/notib/logic/intf/service/LogService.java
+++ notib-service-intf/src/main/java/es/caib/notib/logic/intf/service/LogService.java
@@ -9,18 +9,18 @@ import java.util.concurrent.BlockingQueue;
 
 public interface LogService {
 
-    @PreAuthorize("hasRole('NOT_SUPER')")
+    @PreAuthorize("hasRole('NOT_COM')")
     List<FitxerInfo> llistarFitxers();
 
-    @PreAuthorize("hasRole('NOT_SUPER')")
+    @PreAuthorize("hasRole('NOT_COM')")
     FitxerContingut getFitxerByNom(String nom);
 
-    @PreAuthorize("hasRole('NOT_SUPER')")
+    @PreAuthorize("hasRole('NOT_COM')")
     void tailLogFile(String filePath);
 
-    @PreAuthorize("hasRole('NOT_SUPER')")
+    @PreAuthorize("hasRole('NOT_COM')")
     BlockingQueue<String> getQueue();
 
-    @PreAuthorize("hasRole('NOT_SUPER')")
+    @PreAuthorize("hasRole('NOT_COM')")
     List<String> readLastNLines(String nomFitxer, Long nLinies);
 }
