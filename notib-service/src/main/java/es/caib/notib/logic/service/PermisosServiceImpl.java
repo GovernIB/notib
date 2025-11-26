d3a60aa1f31f685a8d6b9e1d27d38cd20940fdb3
josepg-limit <josepg@limit.es>
2025-11-26 09:15:28 +0100

    Error null pointer al carregar permisos de proc/ser

--- notib-service/src/main/java/es/caib/notib/logic/service/PermisosServiceImpl.java
+++ notib-service/src/main/java/es/caib/notib/logic/service/PermisosServiceImpl.java
@@ -803,7 +803,7 @@ public class PermisosServiceImpl implements PermisosService {
                     .valor(procSer.getCodi() + ((procSer.getNom() != null && !procSer.getNom().isEmpty()) ? " - " + procSer.getNom() : ""))
                     .organGestor(procSer.getOrganGestor() != null ? procSer.getOrganGestor().getCodi() : "")
                     .organNom(procSer.getOrganGestor() != null ? procSer.getOrganGestor().getNom() : "")
-                    .organId(procSer.getOrganGestor().getId() + "")
+                    .organId(procSer.getOrganGestor() != null ? procSer.getOrganGestor().getId() + "" : "")
                     .comu(procSer.isComu())
                     .actiu(procSer.isActiu()).build());
         }
