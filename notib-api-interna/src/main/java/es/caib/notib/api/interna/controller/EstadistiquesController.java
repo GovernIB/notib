f342a9502b46a4f37dd123da0398c148ce286e04
josepg-limit <josepg@limit.es>
2025-11-26 09:51:29 +0100

    eliminar RequestMapping en EstadistiquesController

--- notib-api-interna/src/main/java/es/caib/notib/api/interna/controller/EstadistiquesController.java
+++ notib-api-interna/src/main/java/es/caib/notib/api/interna/controller/EstadistiquesController.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/consulta/v3")
+//@RequestMapping("/consulta/v3")
 public class EstadistiquesController {
 
     private final EstadisticaService estadisticaService;
