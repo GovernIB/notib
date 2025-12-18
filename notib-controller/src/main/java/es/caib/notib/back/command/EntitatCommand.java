ac46f8948b6a8367c6e659e46d672ccfce41360d
josepg-limit <josepg@limit.es>
2025-12-18 11:36:43 +0100

    #1047 No apareix el logo de l'entitat en la capçalera

--- notib-controller/src/main/java/es/caib/notib/back/command/EntitatCommand.java
+++ notib-controller/src/main/java/es/caib/notib/back/command/EntitatCommand.java
@@ -49,12 +49,12 @@ public class EntitatCommand {
 	private String apiKey;
 	private boolean ambEntregaDeh;
 	private String descripcio;
-//	private MultipartFile logoCap;
-	private String logoCap;
+	private MultipartFile logoCap;
+//	private String logoCap;
 	private String logoCapNom;
 	private boolean eliminarLogoCap;
-	private String logoPeu;
-//	private MultipartFile logoPeu;
+	private MultipartFile logoPeu;
+//	private String logoPeu;
 	private String logoPeuNom;
 	private boolean eliminarLogoPeu;
 	private String colorFons;
@@ -109,30 +109,30 @@ public class EntitatCommand {
 		}
 
         if (dto.getLogoCapBytes() != null) {
-            var base64 = Base64.getEncoder().encodeToString(dto.getLogoCapBytes());
-            try {
+//            var base64 = Base64.getEncoder().encodeToString(dto.getLogoCapBytes());
+//            try {
                 var nom = "logo_cap";
-                var mime = MimeUtils.getMimeTypeFromBase64(base64, nom);
-                var multipartFile = new MockMultipartFile(nom, nom, mime, dto.getLogoCapBytes());
-//                entitat.setLogoCap(multipartFile);
-                entitat.setLogoCap(dto.getLogoCap());
+//                var mime = MimeUtils.getMimeTypeFromBase64(base64, nom);
+//                var multipartFile = new MockMultipartFile(nom, nom, mime, dto.getLogoCapBytes());
+////                entitat.setLogoCap(multipartFile);
+//                entitat.setLogoCap(dto.getLogoCap());
                 entitat.setLogoCapNom(nom);
-            } catch (Exception ex) {
-                log.error("Error obtenint el mime type per el logo_cap");
-            }
+//            } catch (Exception ex) {
+//                log.error("Error obtenint el mime type per el logo_cap");
+//            }
         }
         if (dto.getLogoPeuBytes() != null) {
-            var base64 = Base64.getEncoder().encodeToString(dto.getLogoPeuBytes());
-            try {
+//            var base64 = Base64.getEncoder().encodeToString(dto.getLogoPeuBytes());
+//            try {
                 var nom = "logo_peu";
-                var mime = MimeUtils.getMimeTypeFromBase64(base64, nom);
-                var multipartFile = new MockMultipartFile(nom, nom, mime, dto.getLogoPeuBytes());
-//                entitat.setLogoPeu(multipartFile);
-                entitat.setLogoPeu(dto.getLogoPeu());
+//                var mime = MimeUtils.getMimeTypeFromBase64(base64, nom);
+//                var multipartFile = new MockMultipartFile(nom, nom, mime, dto.getLogoPeuBytes());
+////                entitat.setLogoPeu(multipartFile);
+//                entitat.setLogoPeu(dto.getLogoPeu());
                 entitat.setLogoPeuNom(nom);
-            } catch (Exception ex) {
-                log.error("Error obtenint el mime type per el logo_cap");
-            }
+//            } catch (Exception ex) {
+//                log.error("Error obtenint el mime type per el logo_cap");
+//            }
         }
 		return entitat;
 	}
@@ -141,8 +141,8 @@ public class EntitatCommand {
 		var entitat = ConversioTipusHelper.convertir(this, EntitatDataDto.class);
 		List<TipusDocumentDto> tipusDocuments = new ArrayList<>();
 		var tipusDocumentDefault = new TipusDocumentDto();
-//		entitat.setLogoCapBytes(this.getLogoCap().getBytes());
-//		entitat.setLogoPeuBytes(this.getLogoPeu().getBytes());
+		entitat.setLogoCapBytes(this.getLogoCap().getBytes());
+		entitat.setLogoPeuBytes(this.getLogoPeu().getBytes());
 		if (this.getTipusDocName() != null) {
 			TipusDocumentEnumDto tipusDocumentEnum;
 			TipusDocumentDto tipusDocument;
