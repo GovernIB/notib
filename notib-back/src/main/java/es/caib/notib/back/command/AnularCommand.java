65752482ebe798e3f04404a151816dc09de19327
josepg-limit <josepg@limit.es>
2026-01-05 16:57:51 +0100

    #1027 Afegir integració amb Notifica per a poder anul·lar enviaments

---
+++ notib-back/src/main/java/es/caib/notib/back/command/AnularCommand.java
@@ -0,0 +1,18 @@
+package es.caib.notib.back.command;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class AnularCommand {
+
+    private Long notificacioId;
+    private Long enviamentId;
+    private String motiu;
+    private List<Long> notificacionsId;
+    private List<Long> enviamentsId;
+    private boolean massiu;
+}
