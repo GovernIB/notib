7610582f662f1947496b5de8320d962a08e5d8f9
josepg-limit <josepg@limit.es>
2026-01-09 12:12:10 +0100

    Estandaritzar crides estadístiques

--- notib-ejb/src/main/java/es/caib/notib/ejb/EstadisticaService.java
+++ notib-ejb/src/main/java/es/caib/notib/ejb/EstadisticaService.java
@@ -6,6 +6,7 @@ import es.caib.comanda.ms.estadistica.model.RegistresEstadistics;
 import org.jboss.ejb3.annotation.TransactionTimeout;
 import org.springframework.context.annotation.Primary;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import java.time.LocalDate;
 import java.util.List;
@@ -15,49 +16,58 @@ import java.util.List;
 public class EstadisticaService extends AbstractService<es.caib.notib.logic.intf.service.EstadisticaService> implements es.caib.notib.logic.intf.service.EstadisticaService {
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public boolean generarDadesExplotacio() {
         return getDelegateService().generarDadesExplotacio();
     }
 
     @Override
     @TransactionTimeout(value = 3600)
+    @RolesAllowed({"NOT_COM"})
     public boolean generarDadesExplotacio(LocalDate data) {
         return getDelegateService().generarDadesExplotacio(data);
     }
 
     @Override
     @TransactionTimeout(value = 3600)
+    @RolesAllowed({"NOT_COM"})
     public void generarDadesExplotacio(LocalDate data, LocalDate toDate) {
         getDelegateService().generarDadesExplotacio(data, toDate);
     }
 
     @Override
     @TransactionTimeout(value = 3600)
+    @RolesAllowed({"NOT_COM"})
     public void generarDadesExplotacioBasiques(LocalDate fromDate, LocalDate toDate, boolean regenerar) {
         getDelegateService().generarDadesExplotacioBasiques(fromDate, toDate, regenerar);
     }
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public RegistresEstadistics consultaUltimesEstadistiques() {
         return getDelegateService().consultaUltimesEstadistiques();
     }
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public RegistresEstadistics consultaEstadistiques(LocalDate data) {
         return getDelegateService().consultaEstadistiques(data);
     }
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public List<RegistresEstadistics> consultaEstadistiques(LocalDate dataInici, LocalDate dataFi) {
         return getDelegateService().consultaEstadistiques(dataInici, dataFi);
     }
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public List<DimensioDesc> getDimensions() {
         return getDelegateService().getDimensions();
     }
 
     @Override
+    @RolesAllowed({"NOT_COM"})
     public List<IndicadorDesc> getIndicadors() {
         return getDelegateService().getIndicadors();
     }
