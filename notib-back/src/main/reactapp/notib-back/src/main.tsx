743e55f3bc70f7ac4f0ae81ed2ebb66e7c4e81e0
Josep Gayà <josepg@limit.es>
2026-01-26 08:54:34 +0100

    #1011 Nou selector de rol actiu al front

--- notib-back/src/main/reactapp/notib-back/src/main.tsx
+++ notib-back/src/main/reactapp/notib-back/src/main.tsx
@@ -9,7 +9,7 @@ import App from './App.tsx';
 import { envVar, OidcAuthProvider, ContainerAuthProvider, ResourceApiProvider } from 'reactlib';
 
 LicenseInfo.setLicenseKey(
-    'd7a3848ee04d821438959d61037634beTz0xMDY1ODQsRT0xNzY5Mjk5MTk5MDAwLFM9cHJvLExNPXN1YnNjcmlwdGlvbixQVj1pbml0aWFsLEtWPTI='
+    'e0bde345c6cb2453171a44e15a0c58f5Tz0xMjQ4NTIsRT0xODAxMDk0Mzk5MDAwLFM9cHJvLExNPXN1YnNjcmlwdGlvbixQVj1pbml0aWFsLEtWPTI='
 );
 
 export const envVars = {
