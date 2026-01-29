ba48d8a2d2b1a3929564fc777f6faa1e0214015b
josepg-limit <josepg@limit.es>
2026-01-29 13:20:34 +0100

    #1021 Afegir límits d'enviaments per aplicació

--- notib-service/src/main/java/es/caib/notib/logic/helper/NotificacioHelper.java
+++ notib-service/src/main/java/es/caib/notib/logic/helper/NotificacioHelper.java
@@ -118,9 +118,9 @@ public class NotificacioHelper {
                 return msg;
             }
 
-            var maxEnvMinut =  aplicacio.getMaxEnviamentsMinutLaboral();
             var maxEnvDies =  aplicacio.getMaxEnviamentsDiaLaboral();
             var isHorariLaboral = DatesUtils.isHorariLaboral(aplicacio.getHorariLaboralInici(), aplicacio.getHorariLaboralFi());
+            var maxEnvMinut =  isHorariLaboral ? aplicacio.getMaxEnviamentsMinutLaboral() : aplicacio.getMaxEnviamentsMinutNoLaboral();
             var enviamentsMinutActual = isHorariLaboral ? contadorMinutsLaboral.getOrDefault(aplicacio.getId(), 0)
                     : contadorMinutsNoLaboral.getOrDefault(aplicacio.getId(), 0);
             if (maxEnvMinut <= enviamentsMinutActual) {
