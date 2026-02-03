5892503cbfbe6f62671f2acc814c2952ac113aa2
Josep Gayà <josepg@limit.es>
2026-02-03 12:12:27 +0100

    #1011 Gestió de procediments i serveis

---
+++ notib-back/src/main/reactapp/notib-back/src/pages/servei/ServeiForm.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Grid from '@mui/material/Grid';
+import Badge from '@mui/material/Badge';
+import {
+    FormPage,
+    MuiForm,
+    FormField,
+    MuiFormTabs,
+    MuiFormTabContent,
+    useFormContext,
+} from 'reactlib';
+// import ProcedimentFormTabGrups from './ProcedimentFormTabGrups.tsx';
+// import ProcedimentFormTabPermisos from './ProcedimentFormTabPermisos';
+
+const ProcedimentFormContent: React.FC<{ setSubtitle: (subtitle: string) => void }> = (props) => {
+    const { setSubtitle } = props;
+    const { t } = useTranslation();
+    const { data } = useFormContext();
+    React.useEffect(() => {
+        setSubtitle(data?.codi + ', ' + data?.nom);
+    }, [data]);
+    const grupsTabLabel = (
+        <Badge badgeContent={data.grupsCount} color="primary">
+            {t('page.servei.form.tabs.grups')}
+        </Badge>
+    );
+    const permisosTabLabel = (
+        <Badge badgeContent={data.aclEntryCount} color="primary">
+            {t('page.servei.form.tabs.permisos')}
+        </Badge>
+    );
+    const tabs = [
+        t('page.servei.form.tabs.dades'),
+        { label: grupsTabLabel },
+        { label: permisosTabLabel },
+    ];
+    return (
+        <MuiFormTabs tabs={tabs} tabIndexesWithGrids={[1, 2, 4]}>
+            <MuiFormTabContent index={0} showOnCreate>
+                <Grid container spacing={2}>
+                    <Grid size={4}>
+                        <FormField name="codi" />
+                    </Grid>
+                    <Grid size={8}>
+                        <FormField name="nom" />
+                    </Grid>
+                    <Grid size={4}>
+                        <FormField name="organGestor" />
+                    </Grid>
+                    <Grid size={4}>
+                        <FormField name="retard" />
+                    </Grid>
+                    <Grid size={4}>
+                        <FormField name="caducitat" />
+                    </Grid>
+                    <Grid size={3}>
+                        <FormField name="comu" />
+                    </Grid>
+                    <Grid size={3}>
+                        <FormField name="entregaCie" />
+                    </Grid>
+                    <Grid size={3}>
+                        <FormField name="agrupar" />
+                    </Grid>
+                    <Grid size={12}>
+                        <FormField name="requireDirectPermission" />
+                    </Grid>
+                    <Grid size={12}>
+                        <FormField name="manual" />
+                    </Grid>
+                </Grid>
+            </MuiFormTabContent>
+            {/*<MuiFormTabContent index={2}>*/}
+            {/*    <ProcedimentFormTabGrups />*/}
+            {/*</MuiFormTabContent>*/}
+            {/*<MuiFormTabContent index={3}>*/}
+            {/*    <ProcedimentFormTabPermisos />*/}
+            {/*</MuiFormTabContent>*/}
+        </MuiFormTabs>
+    );
+};
+
+export const ProcedimentForm: React.FC = () => {
+    const { t } = useTranslation();
+    const { id } = useParams();
+    const [subtitle, setSubtitle] = React.useState<string>();
+    return (
+        <FormPage>
+            <MuiForm
+                resourceName="procedimentResource"
+                id={id != null ? parseInt(id) : id}
+                additionalData={{ tipus: 'SERVEI' }}
+                title={
+                    id != null
+                        ? t('page.servei.form.titleUpdate')
+                        : t('page.servei.form.titleCreate')
+                }
+                toolbarSubtitle={id != null ? subtitle : undefined}
+                componentProps={{ style: { height: '100%' } }}
+                commonFieldComponentProps={{ size: 'small' }}>
+                <ProcedimentFormContent setSubtitle={setSubtitle} />
+            </MuiForm>
+        </FormPage>
+    );
+};
+export default ProcedimentForm;
---
+++ notib-back/src/main/reactapp/notib-back/src/pages/servei/ServeiGrid.tsx
@@ -0,0 +1,91 @@
+import { useTranslation } from 'react-i18next';
+import Chip from '@mui/material/Chip';
+import { GridPage, MuiDataGrid } from 'reactlib';
+
+const columns = [
+    {
+        field: 'codi',
+        flex: 1,
+    },
+    {
+        field: 'nom',
+        flex: 4,
+    },
+    {
+        field: 'organGestor',
+        flex: 1,
+    },
+    {
+        field: 'retard',
+        flex: 0.6,
+    },
+    {
+        field: 'caducitat',
+        flex: 0.6,
+    },
+    {
+        field: 'entregaCie',
+        flex: 0.6,
+    },
+    {
+        field: 'comu',
+        flex: 0.6,
+    },
+    {
+        field: 'requireDirectPermission',
+        flex: 0.6,
+    },
+    {
+        field: 'manual',
+        flex: 0.6,
+    },
+    {
+        field: 'actiu',
+        flex: 0.6,
+    },
+    {
+        field: 'grupCount',
+        flex: 0.6,
+        renderCell: (params: any) => {
+            return (
+                <Chip
+                    label={params.value}
+                    color={params.value ? 'primary' : undefined}
+                    size="small"
+                />
+            );
+        },
+    },
+    {
+        field: 'aclEntryCount',
+        flex: 0.6,
+        renderCell: (params: any) => {
+            return (
+                <Chip
+                    label={params.value}
+                    color={params.value ? 'primary' : undefined}
+                    size="small"
+                />
+            );
+        },
+    },
+];
+export const ProcedimentGrid = () => {
+    const { t } = useTranslation();
+    return (
+        <GridPage disableMargins={false}>
+            <MuiDataGrid
+                title={t('page.servei.grid.title')}
+                resourceName="procedimentResource"
+                columns={columns}
+                staticFilter="tipus:'SERVEI'"
+                paginationActive
+                toolbarCreateLink="form"
+                rowLink="form/{{id}}"
+                rowUpdateLink="form/{{id}}"
+            />
+        </GridPage>
+    );
+};
+
+export default ProcedimentGrid;
