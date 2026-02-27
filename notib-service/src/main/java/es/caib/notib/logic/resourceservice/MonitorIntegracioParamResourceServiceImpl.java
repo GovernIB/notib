6485b728be7531c95247429eb5759c4ce50382fc
josepg-limit <josepg@limit.es>
2026-02-27 13:51:21 +0100

    #1011 Monitor integracio params

--- notib-service/src/main/java/es/caib/notib/logic/resourceservice/MonitorIntegracioParamResourceServiceImpl.java
+++ notib-service/src/main/java/es/caib/notib/logic/resourceservice/MonitorIntegracioParamResourceServiceImpl.java
@@ -18,5 +18,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MonitorIntegracioParamResourceServiceImpl extends BaseMutableResourceService<MonitorIntegracioParamResource, Long, MonitorIntegracioParamResourceEntity> implements MonitorIntegracioParamResourceService {
 
-
+//	@Override
+//	protected String additionalSpringFilter(String currentSpringFilter, String[] namedQueries) {
+//		return super.additionalSpringFilter("monitorIntegracio.id", namedQueries);
+//	}
 }
