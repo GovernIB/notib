770fd864ca555fd8ae4cf324fd796b4d9e3ca42b
Josep Gaya <josepg@limit.es>
2026-03-23 09:05:42 +0100

    #1011 Tests de la nova API REST per React

--- notib-service/src/test/java/es/caib/notib/logic/objectes/MassivaFileTest.java
+++ notib-service/src/test/java/es/caib/notib/logic/objectes/MassivaFileTest.java
@@ -44,10 +44,8 @@ public class MassivaFileTest {
 
     @Mock
     private ConfigHelper configHelper;
-
     @Mock
     private MessageHelper messageHelper;
-
     @Mock
     private PluginHelper pluginHelper;
 
@@ -306,4 +304,5 @@ public class MassivaFileTest {
     private byte[] getResourceFileAsBytes(String fileName) throws IOException {
         return Files.readAllBytes(Paths.get(Objects.requireNonNull(getClass().getResource(fileName)).getFile()));
     }
+
 }
