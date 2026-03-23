60a41b33771ba6bdafaf0637872760d472ad5310
Josep Gaya <josepg@limit.es>
2026-03-23 08:36:31 +0100

    #1011 Tests de la nova API REST per React

--- notib-service/src/test/java/es/caib/notib/logic/service/OrganGestorServiceTest.java
+++ notib-service/src/test/java/es/caib/notib/logic/service/OrganGestorServiceTest.java
@@ -35,6 +35,7 @@ import org.springframework.test.util.ReflectionTestUtils;
 @Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class OrganGestorServiceTest {
+
     @Mock
     private ProcedimentRepository procedimentRepository;
     @Mock
