26102f05615a6986e0bd5284eec1dbc1137ac999
Josep Gaya <josepg@limit.es>
2026-03-24 14:01:53 +0000

    Notib2 wip react 1011 test

--- notib-service/src/main/java/es/caib/notib/logic/resourceservice/MonitorIntegracioResourceServiceImpl.java
+++ notib-service/src/main/java/es/caib/notib/logic/resourceservice/MonitorIntegracioResourceServiceImpl.java
@@ -37,7 +37,7 @@ public class MonitorIntegracioResourceServiceImpl extends BaseMutableResourceSer
 		register(MonitorIntegracioResource.REPORT_AGRUPACIONS, new MonitorIntegracioResourceServiceImpl.AgrupacioReportGenerator());
 	}
 
-	private class AgrupacioReportGenerator implements ReportGenerator<MonitorIntegracioResourceEntity, Serializable, MonitorIntegracioResource.MonitorIntegracioAgrupacioItem> {
+	class AgrupacioReportGenerator implements ReportGenerator<MonitorIntegracioResourceEntity, Serializable, MonitorIntegracioResource.MonitorIntegracioAgrupacioItem> {
 		@Override
 		public List<MonitorIntegracioResource.MonitorIntegracioAgrupacioItem> generateData(
 			String code,
