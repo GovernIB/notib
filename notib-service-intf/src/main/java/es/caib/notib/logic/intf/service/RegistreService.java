3c291332b6d1d38d151d034f2f01ea5acc5aaa89
Josep Gaya <josepg@limit.es>
2026-03-30 15:41:51 +0200

    #1011 Proves de remeses amb React

--- notib-service-intf/src/main/java/es/caib/notib/logic/intf/service/RegistreService.java
+++ notib-service-intf/src/main/java/es/caib/notib/logic/intf/service/RegistreService.java
@@ -2,28 +2,9 @@ package es.caib.notib.logic.intf.service;
 
 import es.caib.notib.logic.intf.dto.adviser.sir.RespostaSirAdviser;
 import es.caib.notib.logic.intf.dto.adviser.sir.SirAdviser;
-import es.caib.notib.logic.intf.statemachine.dto.ConsultaSirDto;
-import es.caib.notib.logic.intf.statemachine.events.ConsultaSirRequest;
-import es.caib.notib.logic.intf.statemachine.events.EnviamentRegistreRequest;
-import org.springframework.security.access.prepost.PreAuthorize;
-
-import es.caib.notib.logic.intf.dto.RegistreAnotacioDto;
 
 public interface RegistreService {
 
-	
-	/**
-	 * Registra una sortida..
-	 * @return El procediment creat.
-	 */
-	@PreAuthorize("isAuthenticated()")
-	void registrarSortida(RegistreAnotacioDto registreAnotacio);
-
-	@PreAuthorize("isAuthenticated()")
-	boolean enviarRegistre(EnviamentRegistreRequest enviamentRegistreRequest);
-
-	@PreAuthorize("isAuthenticated()")
-	boolean consultaSir(ConsultaSirRequest consulta);
-
 	RespostaSirAdviser sincronitzarEnviamentSir(SirAdviser adviser);
+
 }
