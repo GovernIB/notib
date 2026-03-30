733bf36814cf1fb870acf26ee32137d3d9c32927
Josep Gaya <josepg@limit.es>
2026-03-30 08:20:17 +0000

    #3206 - Afegir drag and drop de fitxers en l'alta de notificacions

--- notib-back/src/main/reactapp/notib-back/src/pages/servei/ServeiFormTabPermisos.tsx
+++ notib-back/src/main/reactapp/notib-back/src/pages/servei/ServeiFormTabPermisos.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFormContext } from 'reactlib';
 import PermissionGrid from '../../components/PermissionGrid';
-import { FormFieldDataActionType } from '../../../lib/components/form/FormContext';
 import { Grid, Icon, Tooltip } from '@mui/material';
 import GridFormField from '../../components/GridFormField';
 import PermissionGridSwitch from '../../components/PermissionGridSwitch';
@@ -10,7 +9,7 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 const PermissionForm: React.FC = () => {
     const { t } = useTranslation();
-    const { dataDispatchAction, fields } = useFormContext();
+    const { apiRef } = useFormContext();
 
     const doFieldChange = (targetValue: boolean) => {
         const permisos = [
@@ -23,14 +22,7 @@ const PermissionForm: React.FC = () => {
         ];
 
         permisos.forEach((nomPermis) => {
-            dataDispatchAction({
-                type: FormFieldDataActionType.FIELD_CHANGE,
-                payload: {
-                    fieldName: nomPermis,
-                    value: targetValue,
-                    field: fields?.find((f) => f.name === nomPermis),
-                },
-            });
+            apiRef?.current?.setFieldValue?.(nomPermis, targetValue);
         });
     };
 
