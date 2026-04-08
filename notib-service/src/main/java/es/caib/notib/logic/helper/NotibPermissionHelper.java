d4c880889a4cf1eb3cf2b0dc1f7e8d41f7ff6095
Josep Gaya <josepg@limit.es>
2026-04-08 11:56:01 +0200

    #1011 Permisos de creació i consulta de notificacions

--- notib-service/src/main/java/es/caib/notib/logic/helper/NotibPermissionHelper.java
+++ notib-service/src/main/java/es/caib/notib/logic/helper/NotibPermissionHelper.java
@@ -168,7 +168,7 @@ public class NotibPermissionHelper {
 		// Només retorna els procediments/serveis no comuns que existeixen a la base de dades.
 		Long currentEntitatId = userSessionHelper.getCurrentEntitatId();
 		ProcSerTipusEnum procSerTipus = getProcSerTipusForQuery(isServei);
-		return procedimentResourceRepository.findIdsByEntitatIdAndTipusAndIdInAndComuFalse(
+		return procedimentResourceRepository.findIdsByEntitatIdAndTipusAndIdInAndComuFalseAndActiuTrue(
 			currentEntitatId,
 			procSerTipus,
 			idsWithPermission);
@@ -269,7 +269,7 @@ public class NotibPermissionHelper {
 		Long currentEntitatId = userSessionHelper.getCurrentEntitatId();
 		ProcSerTipusEnum procSerTipus = getProcSerTipusForQuery(isServei);
 		Set<Long> organGestorIds = !requireDirectPermission ? organGestorWithProcedimentsComunsPermission() : null;
-		return procedimentOrganGestorResourceRepository.findIdsByOrganGestorEntitatIdAndProcedimentTipusAndDirecteTrueAndProcedimentComuAndOrganGestorIdInAndIdIn(
+		return procedimentOrganGestorResourceRepository.findIdsComprovacioPermisos(
 			currentEntitatId,
 			procSerTipus,
 			requireDirectPermission,
