edbc84f663f98a0045be829b1b8af2a102528056
Josep Gaya <josepg@limit.es>
2026-04-13 12:58:38 +0200

    #1011 Guardar estat del component datagrid en REACT

--- notib-back/src/main/reactapp/notib-back/src/hooks/useDataGrid.tsx
+++ notib-back/src/main/reactapp/notib-back/src/hooks/useDataGrid.tsx
@@ -39,7 +39,6 @@ export const useDatagridFilterProps = (
         </MuiFilter>
     );
     return {
-        loading: autoFindDisabled,
         autoFindDisabled: autoFindDisabled,
         toolbarHideQuickFilter: true as true,
         toolbarAdditionalRow: filterComponent,
