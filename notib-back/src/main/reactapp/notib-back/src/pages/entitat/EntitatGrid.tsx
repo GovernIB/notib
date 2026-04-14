dbc0e80a4d46a1acaad78452751d1744bc5e4469
Josep Gaya <josepg@limit.es>
2026-04-14 09:26:22 +0200

    #1011 Arbre d'òrgans gestors en REACT

--- notib-back/src/main/reactapp/notib-back/src/pages/entitat/EntitatGrid.tsx
+++ notib-back/src/main/reactapp/notib-back/src/pages/entitat/EntitatGrid.tsx
@@ -123,7 +123,7 @@ export const EntitatGrid: React.FC = () => {
     );
     const pageSizeOptionsDataGridProps = useDatagridPageSizeOptionsProps();
     return (
-        <GridPage disableMargins={false}>
+        <GridPage>
             <MuiDataGrid
                 title={t('page.entitats.grid.title')}
                 resourceName="entitatResource"
@@ -133,6 +133,7 @@ export const EntitatGrid: React.FC = () => {
                 persistentStateClearPageSortPropsOnTopLevelRouteChange
                 {...filterDataGridProps}
                 {...pageSizeOptionsDataGridProps}
+                toolbarType="upper"
                 toolbarCreateLink="form"
                 rowLink="form/{{id}}"
                 rowUpdateLink="form/{{id}}"
