73463c5a12185b19e16d2f88a0abcd373421cd90
Josep Gaya <josepg@limit.es>
2026-04-15 07:59:00 +0200

    #1011 Arbre d'òrgans gestors en REACT

--- notib-backend/src/main/java/es/caib/notib/back/base/controller/BaseMutableResourceController.java
+++ notib-backend/src/main/java/es/caib/notib/back/base/controller/BaseMutableResourceController.java
@@ -104,11 +104,13 @@ public abstract class BaseMutableResourceController<R extends Resource<? extends
 						buildSingleResourceLinks(
 								created.getId(),
 								null,
-								true,
+								null,
 								null,
 								resourceApiService.permissionsCurrentUser(
 										getResourceClass(),
-										created.getId())).toArray(new Link[0])));
+										created.getId()),
+								true,
+								true).toArray(new Link[0])));
 	}
 
 	@Override
@@ -140,11 +142,13 @@ public abstract class BaseMutableResourceController<R extends Resource<? extends
 						buildSingleResourceLinks(
 								updated.getId(),
 								null,
-								true,
+								null,
 								null,
 								resourceApiService.permissionsCurrentUser(
 										getResourceClass(),
-										id)).toArray(new Link[0])));
+										id),
+								true,
+								true).toArray(new Link[0])));
 	}
 
 	@Override
@@ -169,11 +173,13 @@ public abstract class BaseMutableResourceController<R extends Resource<? extends
 						buildSingleResourceLinks(
 								updated.getId(),
 								null,
-								true,
+								null,
 								null,
 								resourceApiService.permissionsCurrentUser(
 										getResourceClass(),
-										id)).toArray(new Link[0])));
+										id),
+								true,
+								true).toArray(new Link[0])));
 	}
 
 	@Override
@@ -576,29 +582,40 @@ public abstract class BaseMutableResourceController<R extends Resource<? extends
 	protected List<Link> buildSingleResourceLinks(
 			Serializable id,
 			String[] perspective,
-			boolean withDownloadLink,
 			Link singleResourceSelfLink,
-			ResourcePermissions resourcePermissions) {
+			List<ResourceArtifact> artifactsAll,
+			ResourcePermissions resourcePermissions,
+			boolean withDownloadLink,
+			boolean withEditLinksInputAndOutput) {
 		List<Link> links = super.buildSingleResourceLinks(
 				id,
 				perspective,
-				withDownloadLink,
 				singleResourceSelfLink,
-				resourcePermissions);
+				artifactsAll,
+				resourcePermissions,
+				withDownloadLink,
+				withEditLinksInputAndOutput);
 		Link selfLink = links.stream().
 				filter(l -> l.getRel().value().equals("self")).
 				findFirst().orElse(null);
 		if (selfLink != null) {
 			if (resourcePermissions.isWriteGranted()) {
 				ConfigurableAffordance affordance = Affordances.of(selfLink).
-						afford(FAKE_DEFAULT_TEMPLATE_HTTP_METHOD).
-						withName("default").
-						andAfford(HttpMethod.PUT).
+					afford(FAKE_DEFAULT_TEMPLATE_HTTP_METHOD).
+					withName("default");
+				if (withEditLinksInputAndOutput) {
+					affordance = affordance.andAfford(HttpMethod.PUT).
 						withInputAndOutput(getResourceClass()).
-						withName("update").
-						andAfford(HttpMethod.PATCH).
+						withName("update")
+						.andAfford(HttpMethod.PATCH).
 						withInputAndOutput(getResourceClass()).
 						withName("patch");
+				} else {
+					affordance = affordance.andAfford(HttpMethod.PUT).
+						withName("update")
+						.andAfford(HttpMethod.PATCH).
+						withName("patch");
+				}
 				if (resourcePermissions.isDeleteGranted()) {
 					affordance = affordance.
 							andAfford(HttpMethod.DELETE).
@@ -701,10 +718,12 @@ public abstract class BaseMutableResourceController<R extends Resource<? extends
 	}
 
 	@Override
-	protected List<Link> buildSingleResourceArtifactLinks(Serializable id) {
-		List<Link> superLinks = super.buildSingleResourceArtifactLinks(id);
-		List<ResourceArtifact> artifacts = getReadonlyResourceService().artifactFindAll(null);
-		List<Link> links = artifacts.stream().
+	protected List<Link> buildSingleResourceArtifactLinks(Serializable id, List<ResourceArtifact> artifactsAll) {
+		List<Link> superLinks = super.buildSingleResourceArtifactLinks(id, artifactsAll);
+		List<ResourceArtifact> thisArtifactsAll = artifactsAll != null ?
+			artifactsAll :
+			getReadonlyResourceService().artifactFindAll(null);
+		List<Link> links = thisArtifactsAll.stream().
 				filter(a -> a.getType() == ResourceArtifactType.ACTION && a.getRequiresId() != null && a.getRequiresId()).
 				map(a -> buildActionLinkWithAffordances(a, id)).
 				collect(Collectors.toList());
