6efaa8f7ed0d9be2df0044b0d870fe637e41d12e
Josep Gaya <josepg@limit.es>
2026-04-15 07:59:00 +0200

    #1011 Arbre d'òrgans gestors en REACT

--- notib-service-intf/src/main/java/es/caib/notib/logic/intf/model/OrganGestorResource.java
+++ notib-service-intf/src/main/java/es/caib/notib/logic/intf/model/OrganGestorResource.java
@@ -49,11 +49,17 @@ import java.io.Serializable;
 				@ResourceAccessConstraint(
 					type = ResourceAccessConstraint.ResourceAccessConstraintType.ROLE,
 					roles = { BaseConfig.ROLE_ADMIN })
-			}),
+			}
+		),
 		@ResourceArtifact(
 			type = ResourceArtifactType.FILTER,
 			code = OrganGestorResource.FILTER_CODE,
-			formClass = OrganGestorResource.OrganGestorResourceFilter.class)
+			formClass = OrganGestorResource.OrganGestorResourceFilter.class
+		),
+		@ResourceArtifact(
+			type = ResourceArtifactType.PERSPECTIVE,
+			code = OrganGestorResource.PERSPECTIVE_TREE
+		),
 	},
 	accessConstraints = {
 		@ResourceAccessConstraint(
@@ -74,6 +80,8 @@ import java.io.Serializable;
 	}
 )
 public class OrganGestorResource extends BaseResource<Long> {
+
+	public static final String PERSPECTIVE_TREE = "TREE";
 
 	public static final String DIR3_SYNC_ACTION_CODE = "DIR3_SYNC";
 	public static final String FILTER_CODE = "FILTER_ORGAN_GESTOR";
@@ -114,6 +122,10 @@ public class OrganGestorResource extends BaseResource<Long> {
 	private String codiNom;
 	private String nomPare;
 	private Integer aclEntryCount;
+
+	// Camps de la perspectiva TREE
+	private ResourceReference<OrganGestorResource, Long>[] path;
+	private Long childCount;
 
 	// Camps per emplenar els valors del formulari referent a la entrega CIE
 	private ResourceReference<PagadorCieResource, Long> entregaCiePagadorCie;
