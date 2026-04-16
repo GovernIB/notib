4452141ffae15d1006f147cd403b72306f5aff9e
Josep Gaya <josepg@limit.es>
2026-04-16 16:11:33 +0200

    #1011 Arbre d'òrgans gestors en REACT

--- notib-back/src/main/reactapp/notib-back/lib/components/mui/form/MuiFormTabs.tsx
+++ notib-back/src/main/reactapp/notib-back/lib/components/mui/form/MuiFormTabs.tsx
@@ -23,7 +23,7 @@ export const useFormTabsContext = () => {
 /**
  * Propietats del component MuiFormTabContent.
  */
-interface FormTabContentProps {
+interface FormTabContentProps extends React.HTMLAttributes<HTMLDivElement> {
     /** Índex de la pipella */
     index: number;
     /** Indica si aquesta pipella s'ha de mostrar en els formularis de creació */
@@ -86,10 +86,12 @@ export const MuiFormTabs: React.FC<FormTabsProps> = (props) => {
     const { tabs, tabIndexesWithGrids, initialIndex, onIndexChange, children } = props;
     const theme = useTheme();
     const { id } = useFormContext();
+    const tabsRef = React.useRef<HTMLDivElement | null>(null);
     const [index, setIndex] = React.useState<number>(initialIndex ?? 0);
+    const [insideDialog, setInsideDialog] = React.useState<boolean>(false);
     const { setContentExpandsToAvailableHeight } = useBaseAppContext();
     const gridCheck = (index: number) => {
-        if (tabIndexesWithGrids != null) {
+        if (tabIndexesWithGrids != null && !insideDialog) {
             const isGridTab = tabIndexesWithGrids?.includes(index) ?? false;
             setContentExpandsToAvailableHeight(isGridTab);
         }
@@ -102,6 +104,12 @@ export const MuiFormTabs: React.FC<FormTabsProps> = (props) => {
     React.useEffect(() => {
         index && gridCheck(index);
     }, [index]);
+    React.useEffect(() => {
+        if (tabsRef.current) {
+            const dialogParent = tabsRef.current.closest('.MuiDialog-root');
+            setInsideDialog(!!dialogParent);
+        }
+    }, []);
     const tabsHeightFix = { minHeight: '48px' };
     const context = { index };
     return (
@@ -110,6 +118,7 @@ export const MuiFormTabs: React.FC<FormTabsProps> = (props) => {
                 <Tabs
                     value={index}
                     onChange={handleIndexChange}
+                    ref={tabsRef}
                     sx={{ borderBottom: '1px solid ' + theme.palette.divider }}
                 >
                     {tabs.map((t, i) => {
