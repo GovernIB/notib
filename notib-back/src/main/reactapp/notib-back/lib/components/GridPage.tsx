3baeb8ff28c710e9f8a435a2e0ae41a6061d0a35
Josep Gaya <josepg@limit.es>
2026-04-17 13:14:17 +0200

    #1011 Arbre d'òrgans gestors en REACT

--- notib-back/src/main/reactapp/notib-back/lib/components/GridPage.tsx
+++ notib-back/src/main/reactapp/notib-back/lib/components/GridPage.tsx
@@ -28,7 +28,7 @@ export const GridPage: React.FC<GridPageProps> = (props) => {
     } = useBaseAppContext();
     const [proceed, setProceed] = React.useState<boolean>(contentExpandsToAvailableHeight);
     React.useEffect(() => {
-        if (!proceed && contentExpandsToAvailableHeight) {
+        if (!proceed && contentExpandsToAvailableHeight === !autoHeight) {
             setProceed(true);
         }
     }, [contentExpandsToAvailableHeight]);
@@ -37,15 +37,19 @@ export const GridPage: React.FC<GridPageProps> = (props) => {
         return () => setMarginsDisabled(false);
     }, [disableMargins]);
     React.useEffect(() => {
-        setContentExpandsToAvailableHeight(true);
-        return () => setContentExpandsToAvailableHeight(false);
-    }, []);
+        if (!autoHeight) {
+            setContentExpandsToAvailableHeight(true);
+            return () => setContentExpandsToAvailableHeight(false);
+        } else {
+            setContentExpandsToAvailableHeight(false);
+        }
+    }, [autoHeight]);
     return (
         <div
             style={{
                 ...(!autoHeight
                     ? { display: 'flex', flexDirection: 'column', height: '100%' }
-                    : { paddingBottom: '40px' }),
+                    : { }),
                 ...style,
             }}
         >
