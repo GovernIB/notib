0b9e3cb9690922f548b8413a6007f3f0fdf3404d
Josep Gaya <josepg@limit.es>
2026-04-23 09:42:39 +0000

    Errors apiV3 parametres **. Arreglat problemes amb merge de branques

--- notib-service/src/main/java/es/caib/notib/logic/service/AplicacioServiceImpl.java
+++ notib-service/src/main/java/es/caib/notib/logic/service/AplicacioServiceImpl.java
@@ -197,7 +197,7 @@ public class AplicacioServiceImpl implements AplicacioService {
 					.rebreEmailsNotificacio(dto.getRebreEmailsNotificacio())
 					.emailAlt(dto.getEmailAlt())
 					.rebreEmailsNotificacioCreats(dto.getRebreEmailsNotificacioCreats())
-                    .idioma(idioma)
+					.idioma(idioma)
 					.numElementsPaginaDefecte(dto.getNumElementsPaginaDefecte().name())
 					.entitatDefecte(dto.getEntitatDefecte())
 					.organDefecte(dto.getOrganDefecte())
