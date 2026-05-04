73d2549ed7995a5a28bb4ceb83301c24baebbaa0
Josep Gaya <josepg@limit.es>
2026-05-04 13:09:02 +0000

    #1062 Afegir l'opció "Vols rebre els correus agruparts en un únic correu al dia" al perfil d'usuari

--- notib-persistence/src/main/java/es/caib/notib/persist/entity/UsuariEntity.java
+++ notib-persistence/src/main/java/es/caib/notib/persist/entity/UsuariEntity.java
@@ -92,6 +92,7 @@ public class UsuariEntity implements Serializable {
 
 		rebreEmailsNotificacio = usuari.isRebreEmailsNotificacio();
 		rebreEmailsNotificacioCreats = usuari.isRebreEmailsNotificacioCreats();
+		rebreEmailsAgrupats = usuari.isRebreEmailsAgrupats();
 		idioma = Strings.isNullOrEmpty(usuari.getIdioma()) ? "ca" : usuari.getIdioma();
 		emailAlt = usuari.getEmailAlt();
 		numElementsPaginaDefecte = usuari.numElementsPaginaDefecte;
