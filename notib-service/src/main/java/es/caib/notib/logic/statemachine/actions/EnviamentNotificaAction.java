20333dde26e717b5906041f2511295f26604d891
Josep Gaya <josepg@limit.es>
2026-05-11 08:49:01 +0000

    Canvia columna estat d'html a JSON

--- notib-service/src/main/java/es/caib/notib/logic/statemachine/actions/EnviamentNotificaAction.java
+++ notib-service/src/main/java/es/caib/notib/logic/statemachine/actions/EnviamentNotificaAction.java
@@ -114,7 +114,11 @@ public class EnviamentNotificaAction implements Action<EnviamentSmEstat, Enviame
             var errorDescripcio = StringUtils.truncate("Error al enviar l'event de notificació. " + t.getMessage() + "\n" + ExceptionUtils.getStackTrace(t), 2000);
 
             // Controlar si hi ha enviaments per email
-            notificacio.getEnviaments().stream().filter(e -> e.isPerEmail()).forEach(e -> notificacioEventHelper.addEmailEnviamentEvent(e, true, errorDescripcio, fiReintents));
+            notificacio.getEnviaments().stream().filter(e -> e.isPerEmail()).forEach(e -> {
+
+                var event = notificacioEventHelper.addEmailEnviamentEvent(e, true, errorDescripcio, fiReintents);
+                e.setUltimEvent(event);
+            });
             if (fiReintents && (NotificacioEstatEnumDto.ENVIADA_AMB_ERRORS.equals(notificacio.getEstat()))) {
                 notificacio.updateEstat(NotificacioEstatEnumDto.FINALITZADA_AMB_ERRORS);
             }
