4b35bff549d1754706dd5409697efa23148e8721
Josep Gaya <josepg@limit.es>
2026-05-13 08:38:26 +0000

    Error connexió amb Nexea per WSDL

--- notib-plugin/src/main/java/es/caib/notib/plugin/utils/WsClientHelper.java
+++ notib-plugin/src/main/java/es/caib/notib/plugin/utils/WsClientHelper.java
@@ -13,7 +13,6 @@ import org.apache.wss4j.dom.WSConstants;
 import org.apache.wss4j.dom.handler.WSHandlerConstants;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 import javax.ejb.CreateException;
 import javax.management.InstanceNotFoundException;
@@ -25,7 +24,6 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -36,16 +34,12 @@ import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -156,7 +150,7 @@ public class WsClientHelper<T> {
 		}
 		Service service = null;
 		if (!generateTmpFile) {
-			Service.create(url, qname);
+			service = Service.create(url, qname);
 		} else {
 			try {
 				// fetch main WSDL as DOM
