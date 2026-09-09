1e7bd9714ad54e5df208f134dd32f8c9f186e151
Josep Gayà <josepg@limit.es>
2026-09-09 17:50:46 +0200

    #1011 Revisar formulari de procediment

--- notib-back/src/main/reactapp/notib-back/src/pages/procediment/ProcedimentForm.tsx
+++ notib-back/src/main/reactapp/notib-back/src/pages/procediment/ProcedimentForm.tsx
@@ -29,8 +29,8 @@ const ProcedimentFormContent: React.FC<{ setSubtitle: (subtitle: string) => void
                     <GridFormField size={9} name="nom" />
                     <GridFormField size={6} name="retard" />
                     <GridFormField size={6} name="caducitat" componentProps={{ helperText: 'En dies naturals' }}/>
-                    <GridFormField size={9} name="organGestor" disabled={data?.comu}/>
                     <GridFormField size={3} name="comu" />
+                    {!data?.comu && <GridFormField size={9} name="organGestor" />}
                     {!data?.fieldEntregaCieHidden && (
                         <>
                             <GridFormField size={2} name="entregaCieActiva" />
