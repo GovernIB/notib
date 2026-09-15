5872a2999cf5ca66cf509107f29499073ec77a15
Josep Gayà <josepg@limit.es>
2026-09-15 13:39:11 +0200

    #1011 Solucionats problemes en: càrrega de paisos i províncies

--- notib-service/src/main/java/es/caib/notib/logic/resourceservice/EntregaPostalResourceServiceImpl.java
+++ notib-service/src/main/java/es/caib/notib/logic/resourceservice/EntregaPostalResourceServiceImpl.java
@@ -1,22 +1,54 @@
 package es.caib.notib.logic.resourceservice;
 
 import es.caib.notib.logic.base.service.BaseMutableResourceService;
-import es.caib.notib.logic.intf.model.EntregaCieResource;
+import es.caib.notib.logic.intf.base.model.FieldOption;
 import es.caib.notib.logic.intf.model.EntregaPostalResource;
-import es.caib.notib.logic.intf.resourceservice.EntregaCieResourceService;
 import es.caib.notib.logic.intf.resourceservice.EntregaPostalResourceService;
-import es.caib.notib.persist.resourceentity.EntregaCieResourceEntity;
+import es.caib.notib.logic.intf.service.NotificacioService;
 import es.caib.notib.persist.resourceentity.EntregaPostalResourceEntity;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
- * Implementació del servei d'entregues CIE
+ * Implementació del servei d'entrega postal
  *
  * @author Límit Tecnologies
  */
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class EntregaPostalResourceServiceImpl extends BaseMutableResourceService<EntregaPostalResource, Long, EntregaPostalResourceEntity> implements EntregaPostalResourceService {
+
+	private final NotificacioService notificacioService;
+
+	@PostConstruct
+	public void init() {
+		register(EntregaPostalResource.Fields.domiciliPaisCodiIso, new PaisosFieldOptionsProvider());
+		register(EntregaPostalResource.Fields.domiciliProvinciaCodi, new ProvinciesFieldOptionsProvider());
+	}
+
+	class PaisosFieldOptionsProvider implements FieldOptionsProvider {
+		@Override
+		public List<FieldOption> getOptions(String fieldName, Map<String, String[]> requestParameterMap) {
+			return notificacioService.llistarPaisos().stream().
+					map(p -> new FieldOption(p.getAlfa2Pais(), p.getDescripcioPais())).
+					collect(Collectors.toList());
+		}
+	}
+
+	class ProvinciesFieldOptionsProvider implements FieldOptionsProvider {
+		@Override
+		public List<FieldOption> getOptions(String fieldName, Map<String, String[]> requestParameterMap) {
+			return notificacioService.llistarProvincies().stream().
+					map(p -> new FieldOption(p.getId(), p.getDescripcio())).
+					collect(Collectors.toList());
+		}
+	}
 
 }
